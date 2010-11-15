78ab6caeeb5f9aa1f63c8cfcf7e362bab7f53e29
Sebastian Furth <sebastian.furth@denkbares.com>
2010-11-15 16:45:20 +0000

    * moved default markup related classes (and refactoring)

--- Research/KnowWE-Plugin-Core/src/main/java/de/d3web/we/kdom/renderer/KDOMDepthFontSizeRenderer.java
+++ Research/KnowWE-Plugin-Core/src/main/java/de/d3web/we/kdom/renderer/KDOMDepthFontSizeRenderer.java
@@ -1,17 +1,17 @@
 /*
  * Copyright (C) 2010 Chair of Artificial Intelligence and Applied Informatics
  * Computer Science VI, University of Wuerzburg
- * 
+ *
  * This is free software; you can redistribute it and/or modify it under the
  * terms of the GNU Lesser General Public License as published by the Free
  * Software Foundation; either version 3 of the License, or (at your option) any
  * later version.
- * 
+ *
  * This software is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
  * details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this software; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA, or see the FSF
@@ -24,6 +24,7 @@ import de.d3web.we.kdom.KnowWEArticle;
 import de.d3web.we.kdom.KnowWEObjectType;
 import de.d3web.we.kdom.Section;
 import de.d3web.we.kdom.rendering.KnowWEDomRenderer;
+import de.d3web.we.kdom.rendering.StyleRenderer;
 import de.d3web.we.wikiConnector.KnowWEUserContext;
 
 /**
@@ -31,9 +32,9 @@ import de.d3web.we.wikiConnector.KnowWEUserContext;
  * method of tree structure visualization e.g., for logical expressions like
  * (((a and b) or (c and not(e and f)))) Note: all the nodes beeing part of this
  * tree need to get this renderer (and being delegated to).
- * 
+ *
  * @author Jochen
- * 
+ *
  */
 public class KDOMDepthFontSizeRenderer extends KnowWEDomRenderer<KnowWEObjectType> {
 
