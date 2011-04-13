e6b11534996f8076a3cc3668b71397565b2e3409
Christian Grotheer <grotheer@d3web.svn.invalid>
2011-04-13 08:33:59 +0000

    changed time-event test to work with Rdf2GoSemanticCore

--- Research/KnowWE-Plugin-HermesModule/src/test/java/tests/TimeEventTest.java
+++ Research/KnowWE-Plugin-HermesModule/src/test/java/tests/TimeEventTest.java
@@ -9,20 +9,18 @@ import junit.framework.TestCase;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.openrdf.query.BindingSet;
-import org.openrdf.query.QueryEvaluationException;
-import org.openrdf.query.TupleQueryResult;
+import org.ontoware.aifbcommons.collection.ClosableIterator;
+import org.ontoware.rdf2go.exception.ModelRuntimeException;
+import org.ontoware.rdf2go.model.QueryRow;
 
 import de.d3web.plugin.test.InitPluginManager;
 import de.d3web.we.core.KnowWEArticleManager;
 import de.d3web.we.core.KnowWEEnvironment;
 import de.d3web.we.core.packaging.KnowWEPackageManager;
-import de.d3web.we.core.semantic.ISemanticCore;
-import de.d3web.we.core.semantic.SemanticCoreDelegator;
+import de.d3web.we.core.semantic.rdf2go.Rdf2GoCore;
 import de.d3web.we.hermes.TimeEvent;
 import de.d3web.we.kdom.RootType;
 import de.d3web.we.kdom.semanticAnnotation.SemanticAnnotation;
-import de.knowwe.semantic.sparql.SPARQLUtil;
 import dummies.KnowWETestWikiConnector;
 
 public class TimeEventTest extends TestCase {
@@ -30,7 +28,6 @@ public class TimeEventTest extends TestCase {
 	private KnowWEEnvironment ke;
 	private KnowWEArticleManager am;
 	private RootType type;
-	private ISemanticCore sc;
 
 	@Override
 	@Before
@@ -45,8 +42,6 @@ public class TimeEventTest extends TestCase {
 		ke = KnowWEEnvironment.getInstance();
 		type = ke.getRootType();
 		am = ke.getArticleManager(KnowWEEnvironment.DEFAULT_WEB);
-
-		sc = SemanticCoreDelegator.getInstance();
 	}
 
 	@Test
@@ -56,41 +51,46 @@ public class TimeEventTest extends TestCase {
 
 		String testtopic = "Testpage";
 		List<String> sources = new ArrayList<String>();
-		eventsInput.add(new TimeEvent(
-				"Testing",
-				"!§$&/=?#-.,;:_", 1, sources, "8jv-9jv", "", testtopic));
+		eventsInput.add(new TimeEvent("Testing", "!§$&/=?#-.,;:_", 1, sources, "8jv-9jv", "",
+				testtopic));
 		eventsInput.add(new TimeEvent("MeinTestevent", "Beschreibung dieses Events", 1, sources,
 				"8jv", "", testtopic));
-		eventsInput.add(new TimeEvent("Noch ein Event", "ene Test-Beschreibung", 2, sources, "1jv",
+		eventsInput.add(new TimeEvent("Noch ein Event", "eine Test-Beschreibung", 2, sources,
+				"1jv",
 				"", testtopic));
-		eventsInput.add(new TimeEvent("MeinTestevent", "beschreibung dieses Events", 1, sources,
+		eventsInput.add(new TimeEvent("MeinTestevent", "Beschreibung dieses Events", 1, sources,
 				"8jv", "", testtopic));
 
+		// added line break because the new time event markup works only with a
+		// beginning linebreak
+		String content = "\n";
 		// Create Page-Content from eventsInput
-		String content = "";
 		for (TimeEvent t : eventsInput) {
 			content += "<<" + t.getTitle() + "(" + t.getImportance() + ")\n"
 					+ t.getTime().getEncodedString() + "\n" + t.getDescription() + "\n>>\n\n";
 		}
 
-		// Removing duplicate event
+		System.out.println(content);
+
+		// Removing duplicate event from the input list
 		eventsInput.remove(3);
 
 		ke.processAndUpdateArticleJunit(null, content, testtopic, KnowWEEnvironment.DEFAULT_WEB,
 				type);
 
 		String querystring = "select ?x ?t ?desc ?dd ?imp  where {?x lns:hasDescription ?desc . ?x lns:hasTitle ?t . ?x lns:hasDateDescription ?dd . ?x lns:hasImportance ?imp }";
-		TupleQueryResult result = SPARQLUtil.executeTupleQuery(querystring);
+		ClosableIterator<QueryRow> result = Rdf2GoCore.getInstance().sparqlSelectIt(querystring);
 
 		try {
 			while (result.hasNext()) {
-				BindingSet set = result.next();
+				QueryRow row = result.next();
+				System.out.println("result next");
 
-				String title = set.getBinding("t").getValue().stringValue();
-				String impS = set.getBinding("imp").getValue().stringValue();
-				String desc = set.getBinding("desc").getValue().stringValue();
-				String src = set.getBinding("imp").getValue().stringValue();
-				String dateDesc = set.getBinding("dd").getValue().stringValue();
+				String title = row.getValue("t").toString();
+				String impS = row.getValue("imp").asDatatypeLiteral().getValue().toString();
+				String desc = row.getValue("desc").toString();
+				String src = row.getValue("imp").toString();
+				String dateDesc = row.getValue("dd").toString();
 
 				try {
 					title = URLDecoder.decode(title, "UTF-8");
@@ -100,7 +100,6 @@ public class TimeEventTest extends TestCase {
 					dateDesc = URLDecoder.decode(dateDesc, "UTF-8");
 				}
 				catch (UnsupportedEncodingException e) {
-					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
 
@@ -115,23 +114,24 @@ public class TimeEventTest extends TestCase {
 				TimeEvent te = new TimeEvent(title, desc, imp, new ArrayList<String>(), dateDesc,
 						"", testtopic);
 				eventsOutput.add(te);
+				System.out.println(title + "+++" + desc + "+++" + imp + "+++" + dateDesc + "+++"
+						+ testtopic);
 			}
 		}
 		catch (NumberFormatException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		catch (QueryEvaluationException e) {
+		catch (ModelRuntimeException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
-		// assertTrue("Count of sparqled TimeEvents events is " +
-		// eventsOutput.size() + ", should be "
-		// + eventsInput.size(), eventsInput.size() == eventsOutput.size());
-		// for (TimeEvent t : eventsInput) {
-		// assertTrue("TimeEvent " + t + "could not be found in sparql result",
-		// eventsOutput.contains(t));
-		// }
+		assertTrue("Count of sparqled TimeEvents events is " + eventsOutput.size() + ", should be "
+				+ eventsInput.size(), eventsInput.size() == eventsOutput.size());
+		for (TimeEvent t : eventsInput) {
+			assertTrue("TimeEvent " + t + "could not be found in sparql result",
+					eventsOutput.contains(t));
+		}
 	}
 }
