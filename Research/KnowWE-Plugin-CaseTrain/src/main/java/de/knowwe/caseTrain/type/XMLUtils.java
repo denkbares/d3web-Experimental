6f5bddd2134e9a45694afc6fc2c2d9fd2c056f51
Johannes Dienst <dienst86@d3web.svn.invalid>
2011-05-13 16:15:35 +0000

    * First Version of XMLParser

--- Research/KnowWE-Plugin-CaseTrain/src/main/java/de/knowwe/caseTrain/type/XMLUtils.java
+++ Research/KnowWE-Plugin-CaseTrain/src/main/java/de/knowwe/caseTrain/type/XMLUtils.java
@@ -18,6 +18,7 @@
  */
 package de.knowwe.caseTrain.type;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -25,11 +26,24 @@ import java.util.Map;
 
 import org.jdom.Document;
 import org.jdom.Element;
+import org.jdom.output.Format;
+import org.jdom.output.XMLOutputter;
 
 import de.d3web.we.kdom.KnowWEArticle;
 import de.d3web.we.kdom.Section;
 import de.d3web.we.kdom.Sections;
 import de.d3web.we.kdom.basic.PlainText;
+import de.knowwe.caseTrain.info.Antwort;
+import de.knowwe.caseTrain.info.Antwort.AntwortErklaerung;
+import de.knowwe.caseTrain.info.Antwort.AntwortText;
+import de.knowwe.caseTrain.info.Antworten;
+import de.knowwe.caseTrain.info.Erklaerung;
+import de.knowwe.caseTrain.info.Frage;
+import de.knowwe.caseTrain.info.Frage.FrageGewicht;
+import de.knowwe.caseTrain.info.Frage.FrageText;
+import de.knowwe.caseTrain.info.Frage.FrageTyp;
+import de.knowwe.caseTrain.info.Hinweis;
+import de.knowwe.caseTrain.info.Info;
 import de.knowwe.caseTrain.type.MetaLine.AttributeContent;
 import de.knowwe.caseTrain.type.MetaLine.AttributeName;
 import de.knowwe.caseTrain.type.general.Bild;
@@ -42,23 +56,33 @@ import de.knowwe.caseTrain.type.general.Title;
  * @created 11.05.2011
  */
 public class XMLUtils {
+
 	public static void createXMLFromCase(KnowWEArticle article) {
 		Section<KnowWEArticle> articleSec = article.getSection();
-		Element root = new Element("StartElement");
+		Element root = new Element("Case");
 		Document doc = new Document(root);
 
 		// MetaData
 		XMLUtils.addMetaDataElement(root, articleSec);
 
 		// Einleitung
-		XMLUtils.addIntroOutroElement(root, articleSec, "Intro");
+		XMLUtils.addMixedMultimediaElement(root, articleSec, "Intro");
 
 		// Info Abschnitte
 		XMLUtils.addSectionsElement(root, articleSec);
 
 		// Abschluss
-		XMLUtils.addIntroOutroElement(root, articleSec, "Extro");
+		XMLUtils.addMixedMultimediaElement(root, articleSec, "Extro");
 
+		XMLOutputter fmt = new XMLOutputter();
+		fmt.setFormat( Format.getPrettyFormat() );
+		try {
+			fmt.output( doc, System.out );
+		}
+		catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	/**
@@ -69,29 +93,49 @@ public class XMLUtils {
 	 * 
 	 * @created 11.05.2011
 	 * @param root
-	 * @param articleSec
+	 * @param sec
 	 * @param elementName
 	 */
-	private static void addIntroOutroElement(Element root, Section<KnowWEArticle> articleSec, String elementName) {
+	private static void addMixedMultimediaElement(Element root, Section<?> sec, String elementName) {
 		Element intro = new Element(elementName);
 
-		Section<Einleitung> introSec = Sections.findSuccessor(articleSec, Einleitung.class);
+		Section<?> introSec = null;
+
+		if (elementName.equals("Intro"))
+			introSec = Sections.findSuccessor(sec, Einleitung.class);
+		if (elementName.equals("Extro"))
+			introSec = Sections.findSuccessor(sec, Abschluss.class);
+
+		if (introSec == null) return;
+		XMLUtils.addmmmixedcontent(intro, introSec);
+
+		root.addContent(intro);
+	}
+
+	/**
+	 * 
+	 * 
+	 * @created 12.05.2011
+	 * @param intro
+	 * @param introSec
+	 */
+	private static void addmmmixedcontent(Element intro, Section<?> introSec) {
 		List<Section<?>> contentChildren = introSec.getChildren().get(0).getChildren();
 
 		for (Section<?> sec : contentChildren) {
-			if (sec.get() instanceof Title) {
+			if (sec.get().isType(Title.class)) {
 				Element neu = new Element("Title");
 				neu.addContent(sec.getOriginalText());
 				intro.addContent(neu);
 				continue;
 			}
-			if (sec.get() instanceof PlainText) {
+			if (sec.get().isType(PlainText.class)) {
 				Element neu = new Element("Content");
 				neu.addContent(sec.getOriginalText());
 				intro.addContent(neu);
 				continue;
 			}
-			if(sec.get() instanceof Bild) {
+			if(sec.get().isType(Bild.class)) {
 				Element neu = new Element("MultimediaItem");
 				neu.setAttribute("type", "image");
 				Element url = new Element("URL");
@@ -101,15 +145,14 @@ public class XMLUtils {
 				continue;
 			}
 		}
-
-		root.addContent(intro);
 	}
 
 	/**
 	 * 
 	 * Adds all Info-Abschnitte to root.
 	 * 
-	 * TODO not finished.
+	 * TODO only support SimpleSections
+	 * TODO InfoWahl-Sections
 	 * 
 	 * @created 11.05.2011
 	 * @param root
@@ -118,7 +161,184 @@ public class XMLUtils {
 	private static void addSectionsElement(Element root, Section<KnowWEArticle> articleSec) {
 		Element sections = new Element("Sections");
 
+		List<Section<Info>> infoSecs = new ArrayList<Section<Info>>();
+		Sections.findSuccessorsOfType(articleSec, Info.class, infoSecs);
 
+		for(Section<Info> infoSec : infoSecs) {
+			List<Section<?>> childs = infoSec.getChildren().get(0).getChildren();
+			Element simple = new Element("SimpleSection");
+
+			List<Section<?>> frageChilds = new ArrayList<Section<?>>();
+			Element questions = new Element("Questions");
+
+			for (Section<?> child : childs) {
+
+				// First PlainText+Some Multimedia
+				if (child.get().isType(PlainText.class)) {
+					Element neu = new Element("Content");
+					String te = child.getOriginalText().replaceAll("[\\r\\n]", "");
+					if (te.equals("")) continue;
+					neu.addContent(te);
+					simple.addContent(neu);
+					continue;
+				}
+				if(child.get().isType(Bild.class)) {
+					Element neu = new Element("MultimediaItem");
+					neu.setAttribute("type", "image");
+					Element url = new Element("URL");
+					url.addContent(child.getOriginalText());
+					neu.addContent(url);
+					simple.addContent(neu);
+					continue;
+				}
+
+				if (child.get().isType(Frage.class)) {
+					if (!frageChilds.isEmpty()) {
+						XMLUtils.addQuestionsElement(questions, frageChilds);
+					}
+					frageChilds.clear();
+					frageChilds.add(child);
+					continue;
+				}
+				if ( (child.get().isType(Hinweis.class))
+						|| (child.get().isType(Antworten.class))
+						|| (child.get().isType(Erklaerung.class)) ) {
+					frageChilds.add(child);
+				}
+
+				if(child.get().isType(Title.class)) {
+					Element neu = new Element("Title");
+					neu.addContent(child.getOriginalText());
+					simple.addContent(neu);
+					continue;
+				}
+
+			}
+
+			if (!frageChilds.isEmpty())
+				XMLUtils.addQuestionsElement(questions, frageChilds);
+			simple.addContent(questions);
+			sections.addContent(simple);
+		}
+
+		root.addContent(sections);
+	}
+
+	/**
+	 * Creates the Question-Element from the list children
+	 * Possible children for a question are:
+	 * FrageText/Info/Feedback/Antworten
+	 * 
+	 * @created 12.05.2011
+	 * @param questionss
+	 * @param frageChilds.
+	 */
+	private static void addQuestionsElement(Element questions, List<Section<?>> frageChilds) {
+
+		Section<?> frage = frageChilds.remove(0);
+		Section<?> fragetyp = Sections.findSuccessor(frage, FrageTyp.class);
+		Section<?> frageGewicht = Sections.findSuccessor(frage, FrageGewicht.class);
+		Section<?> frageText = Sections.findSuccessor(frage, FrageText.class);
+		Element question = new Element(fragetyp.getOriginalText() + "Question");
+
+		String weight = frageGewicht.getOriginalText().trim();
+		question.setAttribute("weight", weight);
+		Element text = new Element("Text");
+		text.addContent(frageText.getOriginalText());
+		question.addContent(text);
+
+		for (Section<?> sec : frageChilds) {
+			// Hints koennen immer kommen und element Info
+			// beinhalten Text und Multimedia
+			if (sec.get().isType(Hinweis.class)) {
+				XMLUtils.renderHinweis(question, sec);
+				continue;
+			}
+
+			if(sec.get().isType(Antworten.class)) {
+				XMLUtils.addAntwortenElement(question, sec);
+				continue;
+			}
+
+			// Feedback is Erklaerung
+			if (sec.get().isType(Erklaerung.class)) {
+				Element i = new Element("Info");
+				XMLUtils.addmmmixedcontent(i, sec);
+				continue;
+			}
+		}
+
+		questions.addContent(question);
+	}
+
+	/**
+	 * 
+	 * @created 12.05.2011
+	 * @param question
+	 * @param sec
+	 */
+	@SuppressWarnings("unchecked")
+	private static void addAntwortenElement(Element question, Section<?> sec) {
+		Element ants = new Element("Answers");
+
+		for (Section<?> s : sec.getChildren().get(0).getChildren()) {
+			if (s.get().isType(PlainText.class)) continue;
+			Element answer = new Element("Answer");
+
+			// PosFactor and NegFactor
+			String posFactor = Antwort.getPosFactor((Section<Antwort>)s);
+			answer.setAttribute("posFactor", posFactor);
+			String negFactor = Antwort.getNegFactor((Section<Antwort>)s);
+			answer.setAttribute("negFactor", negFactor);
+
+			Section<AntwortText> text = Sections.findSuccessor(s, AntwortText.class);
+			Element t = new Element("Text");
+			t.addContent(text.getOriginalText());
+			answer.addContent(t);
+
+			//SimpleFeedback
+			Section<AntwortErklaerung> erklaerung = Sections.findSuccessor(s, AntwortErklaerung.class);
+			if (erklaerung != null) {
+				Element t1 = new Element("SimpleFeedback");
+				t1.addContent(text.getOriginalText());
+				answer.addContent(t1);
+			}
+
+			ants.addContent(answer);
+		}
+
+		question.addContent(ants);
+	}
+
+	/**
+	 * Appends a Hinweis/Info
+	 * 
+	 * @created 12.05.2011
+	 * @param frage
+	 * @param infoSec
+	 */
+	private static void renderHinweis(Element frage, Section<?> infoSec) {
+		Element info = new Element("Info");
+
+		for (Section<?> sec : infoSec.getChildren().get(0).getChildren()) {
+			if (sec.get().isType(PlainText.class)) {
+				Element neu = new Element("Content");
+				neu.addContent(sec.getOriginalText());
+				info.addContent(neu);
+				continue;
+			}
+			if(sec.get().isType(Bild.class)) {
+				Element neu = new Element("MultimediaItem");
+				neu.setAttribute("type", "image");
+				Element url = new Element("URL");
+				url.addContent(sec.getOriginalText());
+				neu.addContent(url);
+				info.addContent(neu);
+				continue;
+			}
+		}
+
+		frage.addContent(info);
 	}
 
 	/**
@@ -184,7 +404,7 @@ public class XMLUtils {
 		}
 
 		// add Misc-element with Attributes
-		if (!scoreAtts.isEmpty()) {
+		if (!miscAtts.isEmpty()) {
 			Element misc = new Element("Misc");
 			for (Map.Entry<String,String> e : miscAtts.entrySet()) {
 				misc.setAttribute(e.getKey(), e.getValue());
