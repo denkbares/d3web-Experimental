21384df235fb86b8bce3d4d26bcd79453c484732
Florian Ziegler <ziegler@d3web.svn.invalid>
2011-05-25 18:06:19 +0000

    * some very minor refactoring to use the visualisation of testcases * TestCaseExecutor now uses the same visualisation of its results as the TestCase Plugin

--- Research/KnowWE-Plugin-TestCaseExecutor/src/main/java/de/d3web/we/testcaseexecutor/TestCaseExecutorRunTestcaseAction.java
+++ Research/KnowWE-Plugin-TestCaseExecutor/src/main/java/de/d3web/we/testcaseexecutor/TestCaseExecutorRunTestcaseAction.java
@@ -20,20 +20,20 @@ package de.d3web.we.testcaseexecutor;
 
 import java.io.IOException;
 import java.util.Collection;
+import java.util.LinkedList;
 import java.util.List;
 
 import javax.xml.stream.XMLStreamException;
 
 import de.d3web.core.knowledge.KnowledgeBase;
-import de.d3web.empiricaltesting.RatedTestCase;
 import de.d3web.empiricaltesting.SequentialTestCase;
+import de.d3web.empiricaltesting.TestCase;
 import de.d3web.empiricaltesting.TestPersistence;
-import de.d3web.empiricaltesting.caseAnalysis.functions.Diff;
-import de.d3web.empiricaltesting.caseAnalysis.functions.TestCaseAnalysis;
 import de.d3web.we.action.AbstractAction;
 import de.d3web.we.action.UserActionContext;
 import de.d3web.we.basic.D3webModule;
 import de.d3web.we.core.KnowWEEnvironment;
+import de.d3web.we.testcase.action.TestCaseRunAction;
 import de.d3web.we.wikiConnector.ConnectorAttachment;
 import de.d3web.we.wikiConnector.KnowWEWikiConnector;
 
@@ -74,41 +74,36 @@ public class TestCaseExecutorRunTestcaseAction extends AbstractAction {
 		try {
 			List<SequentialTestCase> testcases =
 					TestPersistence.getInstance().loadCases(selectedAttachment.getInputStream(), kb);
-			// TestCase t = new TestCase();
-			// t.setKb(kb);
-			// t.setRepository(testcases);
-			// TestCaseRunAction action = new TestCaseRunAction();
-			// String test = action.renderTestCaseResult(t);
 
 			StringBuilder result = new StringBuilder();
 			result.append("<div>");
 			result.append(TestCaseExecutorUtils.createHiddenFilenameDiv(fileName));
 			result.append(TestCaseExecutorUtils.createHiddenMasterDiv(master));
 			result.append("</div>");
-			result.append("<div><strong>Results:</strong></div><br />");
+
+			TestCase t = new TestCase();
+			List<SequentialTestCase> repo = new LinkedList<SequentialTestCase>();
 
 			for (SequentialTestCase testcase : testcases) {
 				for (String s : cases) {
+
+					// unnamed TestCases are named TESTCASEEXECUTOR_UNNAMED, but
+					// in the kb they are named ""
 					if (s.equals(TESTCASEEXECUTOR_UNNAMED)) s = "";
 
 					if (testcase.getName().equals(s)) {
-						TestCaseAnalysis analysis = TestCaseAnalysis.getInstance();
-						Diff res = analysis.runAndAnalyze(
-								testcase, kb);
-						String name = testcase.getName().equals("")
-								? "Unnamed Testcase"
-								: testcase.getName();
-						result.append("<strong>");
-						result.append(name);
-						result.append(":<br />");
-						result.append("</strong>");
-						result.append(createOutput(res));
-						result.append("<br />");
+						repo.add(testcase);
 						break;
 					}
 				}
 
 			}
+
+			// render the result
+			t.setKb(kb);
+			t.setRepository(repo);
+			TestCaseRunAction action = new TestCaseRunAction();
+			action.renderTests(context, t);
 
 			// append back button
 			result.append("<br />");
@@ -121,65 +116,6 @@ public class TestCaseExecutorRunTestcaseAction extends AbstractAction {
 			e.printStackTrace();
 		}
 
-	}
-
-	private String createOutput(Diff result) {
-		StringBuilder html = new StringBuilder();
-		if (!result.hasDifferences()) {
-			html.append(renderTestCasePassed(result));
-		}
-		else {
-			html.append(renderTestCaseFailed(result));
-		}
-		return html.toString();
-
-	}
-
-	private String renderTestCasePassed(Diff result) {
-		StringBuilder html = new StringBuilder();
-
-		// TestCase passed text and green bulb
-		html.append("<p>");
-		html.append("<img src='KnowWEExtension/images/green_bulb.gif' width='16' height='16' /> ");
-		html.append("<strong>");
-		html.append("Test passed. No differences found!");
-		html.append("</strong>");
-		html.append("</p>");
-
-		return html.toString();
-	}
-
-	private String renderTestCaseFailed(Diff result) {
-		StringBuilder html = new StringBuilder();
-
-		html.append("<p>");
-		html.append("<img src='KnowWEExtension/images/red_bulb.gif' width='16' height='16' /> ");
-		html.append("<strong>");
-		html.append("Test failed!");
-		html.append("</strong>");
-		html.append("</p>");
-
-		html.append("<strong>Findings:</strong>");
-		for (RatedTestCase rtc : result.getCasesWithDifference()) {
-			if (!rtc.getExpectedFindings().equals(rtc.getFindings())) {
-				html.append("<br />Expected:<br />");
-				html.append(rtc.getExpectedFindings());
-				html.append("<br />but was:<br />");
-				html.append(rtc.getFindings());
-			}
-		}
-
-		html.append("<br /><br /><strong>Solutions:</strong>");
-		for (RatedTestCase rtc : result.getCasesWithDifference()) {
-			if (!rtc.getExpectedSolutions().equals(rtc.getDerivedSolutions())) {
-				html.append("<br />Expected:<br />");
-				html.append(rtc.getExpectedSolutions());
-				html.append("<br />but was:<br />");
-				html.append(rtc.getDerivedSolutions());
-			}
-		}
-
-		return html.toString();
 	}
 
 }
