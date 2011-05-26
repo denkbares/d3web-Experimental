a956168ae015b143b0042ee443cb57758029e152
Albrecht Striffler <albrecht.striffler@denkbares.com>
2011-05-26 16:25:25 +0000

    Minor changes to the display of route and level in the header.

--- Research/d3web-ProKEt/src/main/java/de/d3web/proket/d3web/output/render/HerniaD3webRenderer.java
+++ Research/d3web-ProKEt/src/main/java/de/d3web/proket/d3web/output/render/HerniaD3webRenderer.java
@@ -1,17 +1,17 @@
 /**
  * Copyright (C) 2011 Chair of Artificial Intelligence and Applied Informatics
  * Computer Science VI, University of Wuerzburg
- *
+ * 
  * This is free software; you can redistribute it and/or modify it under the
  * terms of the GNU Lesser General Public License as published by the Free
  * Software Foundation; either version 3 of the License, or (at your option) any
  * later version.
- *
+ * 
  * This software is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
  * details.
- *
+ * 
  * You should have received a copy of the GNU Lesser General Public License
  * along with this software; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA, or see the FSF
@@ -39,7 +39,7 @@ import de.d3web.proket.utils.TemplateUtils;
 /**
  * Basic Renderer Class for d3web-based dialogs. Defines the basic rendering of
  * d3web dialogs and methods, required by all rendering sub-classes.
- *
+ * 
  * TODO CHECK: 1) renderRoot: other dialog types or maybe write specific
  * renderers for each one particularly? Maybe better... 2) renderRoot: basic
  * properties such as header, title, HTML header... 3) check global JS 5)
@@ -47,15 +47,15 @@ import de.d3web.proket.utils.TemplateUtils;
  * specific "dialogs", e.g by defining "hierarchic" in the XML and having
  * HierarchicQuestionnaireRenderer etc used automatically (or if not existing,
  * just return to base renderer.)
- *
+ * 
  * TODO CHECK: what happens for more deeply nested question/f-u question
  * hierarchies? Also check an exit-condition for endless recursion!
- *
+ * 
  * TODO LATER: 1) renderRoot: navigation 4) refactor D3webConnector to
  * class-variable?! 5) makeTables: add varying colspans from the XML
  * specification into this method one day 2) Handle cycles!!! 3) handle MC
  * Questions
- *
+ * 
  * @author Martina Freiberg
  * @created 13.01.2011
  */
@@ -65,7 +65,7 @@ public class HerniaD3webRenderer extends D3webRenderer {
 	 * Retrieves the appropriate renderer class according to what base object is
 	 * given from the d3web knowledge base. EXCLUDES answers, as those need a
 	 * specific handling.
-	 *
+	 * 
 	 * @created 14.01.2011
 	 * @param to the TerminologyObject that needs to retrieve the renderer.
 	 * @return the suiting renderer class
@@ -81,7 +81,7 @@ public class HerniaD3webRenderer extends D3webRenderer {
 	/**
 	 * Retrieves the appropriate renderer class for answers,according to what
 	 * base object (question type) is given.
-	 *
+	 * 
 	 * @created 15.01.2011
 	 * @param to the TerminologyObject that needs to retrieve the answer
 	 *        renderer.
@@ -99,7 +99,7 @@ public class HerniaD3webRenderer extends D3webRenderer {
 	/**
 	 * Retrieves the renderer for the Unknown object (unknown option for
 	 * dialogs).
-	 *
+	 * 
 	 * @created 23.01.2011
 	 * @return the suiting renderer class.
 	 */
@@ -171,31 +171,32 @@ public class HerniaD3webRenderer extends D3webRenderer {
 		}
 
 		/* HERNIA specific */
-		Question route =
-				D3webConnector.getInstance().getKb().getManager()
-						.searchQuestion("Please select your hernia route");
+		// route
+		Question route = D3webConnector.getInstance().getKb().getManager().searchQuestion(
+				"Please select your hernia route");
+		Value rVal = D3webRenderer.d3webSession.getBlackboard().getValue(route);
 
-		Value val =
-				D3webRenderer.d3webSession.getBlackboard().getValue(route);
-
-		if (val != null && UndefinedValue.isNotUndefinedValue(val)) {
-			st.setAttribute("route", val.toString().replace(" route", ""));
-		}
-		else {
-			st.setAttribute("route", " ");
-		}
 		// level
-		Question level =
-				D3webConnector.getInstance().getKb().getManager()
-						.searchQuestion("Please choose the database level");
+		Question level = D3webConnector.getInstance().getKb().getManager().searchQuestion(
+				"Please choose the database level");
+		Value lVal = D3webRenderer.d3webSession.getBlackboard().getValue(level);
 
-		Value val2 =
-				D3webRenderer.d3webSession.getBlackboard().getValue(level);
-		if (val2 != null && UndefinedValue.isNotUndefinedValue(val2)) {
-			st.setAttribute("level", val2.toString().replace("Level ", ""));
+		if ((rVal != null && UndefinedValue.isNotUndefinedValue(rVal)) || (lVal != null
+				&& UndefinedValue.isNotUndefinedValue(lVal))) {
+			String infoString = "";
+			if (lVal != null && UndefinedValue.isNotUndefinedValue(lVal)) {
+				infoString += "Level: " + lVal.toString().replace("Level ", "");
+			}
+			if (rVal != null && UndefinedValue.isNotUndefinedValue(rVal)) {
+				if (!infoString.isEmpty()) {
+					infoString += ", ";
+				}
+				infoString += "Route: " + rVal.toString().replace(" route", "");
+			}
+			st.setAttribute("info", infoString);
 		}
 		else {
-			st.setAttribute("level", " ");
+			st.setAttribute("info", "");
 		}
 
 		// add some buttons for basic functionality
