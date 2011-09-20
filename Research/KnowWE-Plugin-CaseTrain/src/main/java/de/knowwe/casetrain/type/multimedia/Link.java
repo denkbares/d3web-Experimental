d880eb769f0b26e6ff699f796c0b4e05093dafd6
Albrecht Striffler <albrecht.striffler@denkbares.com>
2011-09-20 20:21:47 +0000

    Adapted due to changes in KnowWE.

--- Research/KnowWE-Plugin-CaseTrain/src/main/java/de/knowwe/casetrain/type/multimedia/Link.java
+++ Research/KnowWE-Plugin-CaseTrain/src/main/java/de/knowwe/casetrain/type/multimedia/Link.java
@@ -1,20 +1,20 @@
 /*
  * Copyright (C) 2011 University Wuerzburg, Computer Science VI
- *
- * This is free software; you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License as
- * published by the Free Software Foundation; either version 3 of
- * the License, or (at your option) any later version.
- *
- * This software is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this software; if not, write to the Free
- * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
- * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ * 
+ * This is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 3 of the License, or (at your option) any
+ * later version.
+ * 
+ * This software is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this software; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA, or see the FSF
+ * site: http://www.fsf.org.
  */
 package de.knowwe.casetrain.type.multimedia;
 
@@ -26,8 +26,6 @@ import de.d3web.we.kdom.rendering.KnowWEDomRenderer;
 import de.d3web.we.kdom.sectionFinder.RegexSectionFinder;
 import de.d3web.we.user.UserContext;
 import de.d3web.we.utils.KnowWEUtils;
-
-
 
 /**
  * 
@@ -45,7 +43,7 @@ public class Link extends MultimediaItem {
 	public Link() {
 		super(REGEX);
 
-		this.subtreeHandler.clear();
+		this.clearSubtreeHandlers();
 		this.addChildType(new Url());
 
 		this.setCustomRenderer(new KnowWEDomRenderer<Link>() {
@@ -56,27 +54,29 @@ public class Link extends MultimediaItem {
 				Section<MultimediaItemContent> linkURL = Sections.findChildOfType(sec,
 						MultimediaItemContent.class);
 				string.append(KnowWEUtils.maskHTML(
-				"<span title=\"Link\" class=\"casetrainlink\">"));
+						"<span title=\"Link\" class=\"casetrainlink\">"));
 				Section<Url> url = Sections.findChildOfType(sec, Url.class);
 				string.append(KnowWEUtils.maskHTML("<a href=\""));
 				String linkString = linkURL.getOriginalText().trim();
-				if (!linkString.startsWith("http://"));
+				if (!linkString.startsWith("http://"))
+				;
 				string.append("http://");
 				string.append(linkString);
 				string.append(KnowWEUtils.maskHTML("\">"));
 				if (url == null)
 					string.append(linkURL.getOriginalText().trim());
-				else
+					else
 					string.append(url.getOriginalText().
-							substring(1, url.getOriginalText().length()-1).trim());
-				string.append(KnowWEUtils.maskHTML("</a>"));
-				string.append(KnowWEUtils.maskHTML("</span>"));
-			}
+							substring(1, url.getOriginalText().length() - 1).trim());
+					string.append(KnowWEUtils.maskHTML("</a>"));
+					string.append(KnowWEUtils.maskHTML("</span>"));
+				}
 		});
 
 	}
 
 	public class Url extends AbstractType {
+
 		private final String regex = "\\{.*?\\}";
 
 		public Url() {
