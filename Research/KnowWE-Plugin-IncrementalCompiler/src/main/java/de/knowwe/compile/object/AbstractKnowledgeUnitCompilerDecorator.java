9d882c36e32276cf509893ae59e0ff25c1d137ac
Jochen Reutelshöfer <jochen.reutelshoefer@denkbares.com>
2011-11-01 20:16:16 +0000

    ExtensionPoint for CompileScripts active

--- Research/KnowWE-Plugin-IncrementalCompiler/src/main/java/de/knowwe/compile/object/AbstractKnowledgeUnitCompilerDecorator.java
+++ Research/KnowWE-Plugin-IncrementalCompiler/src/main/java/de/knowwe/compile/object/AbstractKnowledgeUnitCompilerDecorator.java
@@ -9,40 +9,32 @@ import de.knowwe.core.kdom.parsing.Section;
 import de.knowwe.plugin.Plugins;
 
 public class AbstractKnowledgeUnitCompilerDecorator extends AbstractType
-		implements KnowledgeUnitCompileScript {
+		implements KnowledgeUnit {
 
 	private KnowledgeUnitCompileScript compileScript = null;
 
 	public AbstractKnowledgeUnitCompilerDecorator() {
-//		Extension[] exts = PluginManager.getInstance().getExtensions(
-//				Plugins.EXTENDED_PLUGIN_ID,
-//				Plugins.EXTENDED_POINT_COMPILESCRIPT);
-//		for (Extension extension : exts) {
-//			String parameter = extension.getParameter("scope");
-//			String thisClassName = this.getClass().getCanonicalName();
-//			if (parameter.equals(thisClassName)) {
-//				Object o = extension.getSingleton();
-//				if (o instanceof KnowledgeUnitCompileScript) {
-//					compileScript = (((KnowledgeUnitCompileScript) o));
-//				}
-//			}
-//		}
+		Extension[] exts = PluginManager.getInstance().getExtensions(
+				Plugins.EXTENDED_PLUGIN_ID,
+				Plugins.EXTENDED_POINT_COMPILESCRIPT);
+		for (Extension extension : exts) {
+			String parameter = extension.getParameter("scope");
+			String thisClassName = this.getClass().getCanonicalName();
+			if (parameter.equals(thisClassName)) {
+				Object o = extension.getSingleton();
+				if (o instanceof KnowledgeUnitCompileScript) {
+					compileScript = (((KnowledgeUnitCompileScript) o));
+				}
+			}
+		}
 	}
 
-	@Override
-	public Collection getAllReferencesOfKnowledgeUnit(Section section) {
-		return compileScript.getAllReferencesOfKnowledgeUnit(section);
-	}
+
 
 	@Override
-	public void insertIntoRepository(Section section) {
-		compileScript.insertIntoRepository(section);
-
-	}
-
-	@Override
-	public void deleteFromRepository(Section section) {
-		compileScript.deleteFromRepository(section);
+	public KnowledgeUnitCompileScript getCompileScript() {
+		// TODO Auto-generated method stub
+		return compileScript;
 	}
 
 }
