b65fd39fe05f877d8ba773d18d95d903371766b8
Albrecht Striffler <albrecht.striffler@denkbares.com>
2011-11-18 10:48:10 +0000

    Adjusted project version.

--- Research/KnowWE-Plugin-d3web-SimpleScoreTable/src/main/java/de/knowwe/d3web/scoreTable/ScoreTableCellSubtreeHandler.java
+++ Research/KnowWE-Plugin-d3web-SimpleScoreTable/src/main/java/de/knowwe/d3web/scoreTable/ScoreTableCellSubtreeHandler.java
@@ -11,7 +11,6 @@ import de.d3web.core.manage.RuleFactory;
 import de.d3web.scoring.ActionHeuristicPS;
 import de.d3web.scoring.Score;
 import de.d3web.scoring.inference.PSMethodHeuristic;
-import de.d3web.we.kdom.condition.CompositeCondition;
 import de.d3web.we.kdom.condition.KDOMConditionFactory;
 import de.d3web.we.reviseHandler.D3webSubtreeHandler;
 import de.d3web.we.utils.D3webUtils;
@@ -24,60 +23,60 @@ import de.knowwe.core.utils.KnowWEUtils;
 import de.knowwe.report.message.NoSuchObjectError;
 import de.knowwe.report.message.ObjectCreatedMessage;
 
-public class ScoreTableCellSubtreeHandler extends D3webSubtreeHandler<ScoreCell>{
-	
+public class ScoreTableCellSubtreeHandler extends D3webSubtreeHandler<ScoreCell> {
+
 	private static final String ruleStoreKey = "rule-store-key";
-	
+
 	public ScoreTableCellSubtreeHandler() {
-		//this.setIgnorePackageCompile(true);
+		// this.setIgnorePackageCompile(true);
 	}
 
 	@Override
 	public Collection<KDOMReportMessage> create(KnowWEArticle article,
 			Section<ScoreCell> section) {
-		
-		
-		
+
 		List<KDOMReportMessage> messages = new ArrayList<KDOMReportMessage>();
-		
-		if(section.getOriginalText().trim().length() == 0) {
+
+		if (section.getOriginalText().trim().length() == 0) {
 			return messages;
 		}
-		
+
 		// line of current score-cell
 		Section<ContentLine> line = Sections.findAncestorOfType(section, ContentLine.class);
-		
+
 		// condition at beginning of that line
 		Section<CondCell> condition = Sections.findSuccessor(line, CondCell.class);
-		
-		//column index to calculate solution cell
+
+		// column index to calculate solution cell
 		int columnIndex = Sections.findSuccessorsOfType(line, ScoreCell.class).indexOf(section);
-		Section<SimpleScoreTable> table = Sections.findAncestorOfExactType(section, SimpleScoreTable.class);
+		Section<SimpleScoreTable> table = Sections.findAncestorOfExactType(section,
+				SimpleScoreTable.class);
 		Section<HeaderLine> headerLine = Sections.findChildOfType(table, HeaderLine.class);
-		
-		List<Section<SolutionCell>> solutionCells = Sections.findSuccessorsOfType(headerLine, SolutionCell.class);
+
+		List<Section<SolutionCell>> solutionCells = Sections.findSuccessorsOfType(headerLine,
+				SolutionCell.class);
 		Section<? extends Type> solutionCell = solutionCells.get(columnIndex);
 		Section<Solution> solution = Sections.findSuccessor(solutionCell, Solution.class);
-		
-		
-		if(solution == null) {
+
+		if (solution == null) {
 			messages.add(new NoSuchObjectError("Solution not found"));
 		}
-		if( condition == null) {
+		if (condition == null) {
 			messages.add(new NoSuchObjectError("Condition not found"));
 		}
 
-		if(solution != null && condition != null) {
-			de.d3web.core.knowledge.terminology.Solution s = solution.get().getTermObject(article, solution);
-			
+		if (solution != null && condition != null) {
+			de.d3web.core.knowledge.terminology.Solution s = solution.get().getTermObject(article,
+					solution);
+
 			Condition d3webCond = KDOMConditionFactory.createCondition(article, condition);
-			
+
 			Score score = D3webUtils.getScoreForString(section.getOriginalText().trim());
-			if (solution == null || score == null) return null;
+			if (solution == null || score == null) return messages;
 			ActionHeuristicPS a = new ActionHeuristicPS();
 			a.setSolution(s);
 			a.setScore(score);
-			
+
 			Rule r = RuleFactory.createRule(a, d3webCond,
 					null, PSMethodHeuristic.class);
 			if (r != null) {
@@ -87,8 +86,7 @@ public class ScoreTableCellSubtreeHandler extends D3webSubtreeHandler<ScoreCell>
 								"Rule"));
 			}
 		}
-		
-		
+
 		return messages;
 	}
 
