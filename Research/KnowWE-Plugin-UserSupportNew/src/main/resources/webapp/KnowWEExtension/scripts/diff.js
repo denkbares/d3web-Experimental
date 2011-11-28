6ea8b60f1cab80f05ab354d77ce5a62bfed1ebca
Johannes Dienst <dienst86@d3web.svn.invalid>
2011-11-28 10:59:14 +0000

    * styling of rules complete * more styling of tables

--- Research/KnowWE-Plugin-UserSupportNew/src/main/resources/webapp/KnowWEExtension/scripts/diff.js
+++ Research/KnowWE-Plugin-UserSupportNew/src/main/resources/webapp/KnowWEExtension/scripts/diff.js
@@ -1,7 +1,8 @@
 CodeMirror.defineMode("diff", function() {
 
-	var keywords = ("IF AND OR NOT THEN KNOWN UNKNOWN").split(" ");
-	var brackets = ("{ } ( ) | [ ]").split(" ");
+	var keywords = ("IF WENN THEN DANN AND UND & OR ODER | NOT NICHT ! KNOWN UNKNOWN AUSSER EXCEPT").split(" ");
+	var brackets = ("{ } ( ) | [ ] ,").split(" ");
+	var d3webScorings = ("P7 P6 P5x P5 P4 P3 P2 P1 N1 N2 N3 N4 N5 N5x N6 N7 ESTABLISHED ETABLIERT SUGGESTED VERDAECHTIGT ++ + x YES JA NO NEIN -").split(" ");
 
 	function arrayContains(arr, item) {
 		if (!Array.prototype.indexOf) {
@@ -21,13 +22,15 @@ CodeMirror.defineMode("diff", function() {
 		
 		// if stream starts with whitespaces
 		while (stream.peek() == " ")
-			stream.next();
-			
+			token = stream.next();
+		if (token.length != 0 && token.charAt(0) == ' ')
+			return "whitespaces";
+		
 		// brackets and equals
 		if (!stream.eol()) {
 			token = stream.next();
 			if (arrayContains(brackets, token) || token == "=")
-				return token;
+				return "bracket";
 		}
 		
 		// compute the token
@@ -42,13 +45,17 @@ CodeMirror.defineMode("diff", function() {
     token: function(stream) {
       var token = tokenize(stream);
       
+      // whitespaces
+      if (token == "whitespaces")
+    	  return "whitespaces";
+      
 	  // equals
 	  if (token == "=")
 		return "equals";
 	  
 	  // brackets or delimiter
-	  if (arrayContains(brackets, token))
-		return "bracket";
+	  if (token == "bracket")
+		return token;
 	  
 	  // line is a comment
 	  if (token.indexOf("//") == 0) {
@@ -56,8 +63,13 @@ CodeMirror.defineMode("diff", function() {
 		return "comment";
 	  }
 	  
+	  // keyword
       if (arrayContains(keywords, token))
 		return "keyword";
+		
+	  // scoring
+	  if (arrayContains(d3webScorings, token))
+	  	return "scoring";
 		
 	  return "term";
     }	
