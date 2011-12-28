13670afddc037df0adf84c954652438d48aa8823
Albrecht Striffler <albrecht.striffler@denkbares.com>
2011-12-28 12:16:47 +0000

    * Added svn ignore for EuraHS-Data. * Fixed minor logging bug.

--- Research/d3web-ProKEt/src/main/java/de/d3web/proket/d3web/ue/log/JSONLogger.java
+++ Research/d3web-ProKEt/src/main/java/de/d3web/proket/d3web/ue/log/JSONLogger.java
@@ -19,7 +19,6 @@
  */
 package de.d3web.proket.d3web.ue.log;
 
-import de.d3web.proket.utils.GlobalSettings;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
@@ -31,6 +30,7 @@ import java.util.LinkedHashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.JSONValue;
@@ -38,10 +38,12 @@ import org.json.simple.parser.ContainerFactory;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
+import de.d3web.proket.utils.GlobalSettings;
+
 /**
- * Basic class for writing logfiles in the JSON format.
- * Collects values to log in internal JSON representation.
- * Can write JSONObject into corresponding textfile.
+ * Basic class for writing logfiles in the JSON format. Collects values to log
+ * in internal JSON representation. Can write JSONObject into corresponding
+ * textfile.
  * 
  * TODO: Umlauts!
  * 
@@ -50,202 +52,217 @@ import org.json.simple.parser.ParseException;
  */
 public class JSONLogger {
 
-    private JSONObject logfile;
-    private JSONArray clickarray;
-    
-    protected static final String BROWSER = "browser";
-    protected static final String USER = "user";
-    protected static final String RESULT = "result";
-    protected static final String START = "start";
-    protected static final String END = "end";
-    protected static final String CLICKED = "clickedwidgets";
-    protected static final String ID = "id";
-    protected static final String TS = "timestamp";
-    protected static final String VAL = "value";
-    private static final long serialVersionUID = -5766536853041423918L;
+	private final JSONObject logfile;
+	private final JSONArray clickarray;
 
-    /**
-     * Constructor of JSONLogger class; due to Singleton pattern private-only.
-     */
-    public JSONLogger() {
-        logfile = new JSONObject();
-        clickarray = new JSONArray();
-    }
+	protected static final String BROWSER = "browser";
+	protected static final String USER = "user";
+	protected static final String RESULT = "result";
+	protected static final String START = "start";
+	protected static final String END = "end";
+	protected static final String CLICKED = "clickedwidgets";
+	protected static final String ID = "id";
+	protected static final String TS = "timestamp";
+	protected static final String VAL = "value";
+	private static final long serialVersionUID = -5766536853041423918L;
 
-    /**
-     * Adds the given value to the internal JSONObject representation.
-     * @param value the value to be added
-     */
-    public void logBrowserValue(String value) {
-        logfile.put(BROWSER, value);
-    }
+	/**
+	 * Constructor of JSONLogger class; due to Singleton pattern private-only.
+	 */
+	public JSONLogger() {
+		logfile = new JSONObject();
+		clickarray = new JSONArray();
+	}
 
-    /**
-     * Adds the given value to the internal JSONObject representation.
-     * @param value the value to be added
-     */
-    public void logUserValue(String value) {
-        logfile.put(USER, value);
-    }
+	/**
+	 * Adds the given value to the internal JSONObject representation.
+	 * 
+	 * @param value the value to be added
+	 */
+	public void logBrowserValue(String value) {
+		logfile.put(BROWSER, value);
+	}
 
-    /**
-     * Adds the given value to the internal JSONObject representation.
-     * @param value the value to be added
-     */
-    public void logResultValue(String value) {
-        logfile.put(RESULT, value);
-    }
+	/**
+	 * Adds the given value to the internal JSONObject representation.
+	 * 
+	 * @param value the value to be added
+	 */
+	public void logUserValue(String value) {
+		logfile.put(USER, value);
+	}
 
-    /**
-     * Adds the given value to the internal JSONObject representation.
-     * @param value the value to be added
-     */
-    public void logStartValue(String value) {
-        logfile.put(START, value);
-    }
+	/**
+	 * Adds the given value to the internal JSONObject representation.
+	 * 
+	 * @param value the value to be added
+	 */
+	public void logResultValue(String value) {
+		logfile.put(RESULT, value);
+	}
 
-    /**
-     * Adds the given value to the internal JSONObject representation.
-     * @param value the value to be added
-     */
-    public void logEndValue(String value) {
-        logfile.put(END, value);
-    }
+	/**
+	 * Adds the given value to the internal JSONObject representation.
+	 * 
+	 * @param value the value to be added
+	 */
+	public void logStartValue(String value) {
+		logfile.put(START, value);
+	}
 
-    /**
-     * Adds a new JSONObject representing a clicked widget to the 
-     * clicked-objects-JSONArray of the JSON logfile. 
-     * Therefore, first a new JSONObject is created and then added to the 
-     * JSONArray of the JSON logfile.
-     * 
-     * @param id for the JSONObject to be added to the array of the JSON 
-     * logfile.
-     * @param timestamp timestamp for the JSONObject to be added to the array 
-     * of the JSON logfile.
-     * @param value value for the JSONObject to be added to the array of the 
-     * JSON logfile.
-     */
-    public void logClickedObjects(Object id, Object timestamp, Object value) {
+	/**
+	 * Adds the given value to the internal JSONObject representation.
+	 * 
+	 * @param value the value to be added
+	 */
+	public void logEndValue(String value) {
+		logfile.put(END, value);
+	}
 
-        JSONObject ob = new JSONObject();
-        ob.put(ID, id);
-        ob.put(TS, timestamp);
-        ob.put(VAL, value);
-        
-        JSONArray existingClickedObjects = getClickedObjects();
-        existingClickedObjects.add(ob);
+	/**
+	 * Adds a new JSONObject representing a clicked widget to the
+	 * clicked-objects-JSONArray of the JSON logfile. Therefore, first a new
+	 * JSONObject is created and then added to the JSONArray of the JSON
+	 * logfile.
+	 * 
+	 * @param id for the JSONObject to be added to the array of the JSON
+	 *        logfile.
+	 * @param timestamp timestamp for the JSONObject to be added to the array of
+	 *        the JSON logfile.
+	 * @param value value for the JSONObject to be added to the array of the
+	 *        JSON logfile.
+	 */
+	public void logClickedObjects(Object id, Object timestamp, Object value) {
 
-        logfile.put(CLICKED, existingClickedObjects);
-    }
+		JSONObject ob = new JSONObject();
+		ob.put(ID, id);
+		ob.put(TS, timestamp);
+		ob.put(VAL, value);
 
-    // get the existing clicked objects already stored in the internal array
-    private JSONArray getClickedObjects(){
-        return clickarray;
-    }
-    
-    /**
-     * Retrieve the already stored logdata as JSONObject
-     * @return the JSONObject representation of the logged data
-     */
-    public JSONObject getLogAsJSON() {
-        return logfile;
-    }
+		JSONArray existingClickedObjects = getClickedObjects();
+		existingClickedObjects.add(ob);
 
-    /**
-     * Write the logdata currently stored internally in the JSONObject into a 
-     * specified file.
-     * @param file the file, the logdata is written to.
-     */
-    public void writeJSONToFile(String file) {
-        BufferedWriter bw = null;
+		logfile.put(CLICKED, existingClickedObjects);
+	}
 
-        try {
-            File dir = new File(GlobalSettings.getInstance().getLogFolder() + "/");
-            if (!dir.exists()) {
-                dir.mkdir();
-            }
-            String filepath = dir + "/" + file;
-            
-            bw = new BufferedWriter(new FileWriter(filepath));
-            bw.write(getLogAsJSON().toString());
+	// get the existing clicked objects already stored in the internal array
+	private JSONArray getClickedObjects() {
+		return clickarray;
+	}
 
-        } catch (FileNotFoundException ex) {
-            ex.printStackTrace();
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        } finally {
-            //Close the BufferedWriter
-            try {
-                if (bw != null) {
-                    bw.flush();
-                    bw.close();
-                }
-            } catch (IOException ex) {
-                ex.printStackTrace();
-            }
-        }
-    }
+	/**
+	 * Retrieve the already stored logdata as JSONObject
+	 * 
+	 * @return the JSONObject representation of the logged data
+	 */
+	public JSONObject getLogAsJSON() {
+		return logfile;
+	}
 
-    public static void main(String[] args) {
-        JSONLogger logger = new JSONLogger();
-        logger.logBrowserValue("firefox");
-        logger.logUserValue("user");
-        logger.logResultValue("result");
-        logger.logStartValue("start");
-        logger.logEndValue("end");
-        System.out.println(logger.getLogAsJSON());
+	/**
+	 * Write the logdata currently stored internally in the JSONObject into a
+	 * specified file.
+	 * 
+	 * @param file the file, the logdata is written to.
+	 */
+	public void writeJSONToFile(String file) {
+		BufferedWriter bw = null;
 
-        logger.logClickedObjects("id1", "now", "answer1");
-        System.out.println(logger.getLogAsJSON());
+		try {
+			File dir = new File(GlobalSettings.getInstance().getLogFolder() + "/");
+			if (!dir.exists()) {
+				dir.mkdirs();
+			}
+			String filepath = dir + "/" + file;
 
-        logger.logClickedObjects("id2", "nowlater", "answer2");
-        System.out.println(logger.getLogAsJSON());
+			bw = new BufferedWriter(new FileWriter(filepath));
+			bw.write(getLogAsJSON().toString());
 
-        logger.writeJSONToFile("TESTI.txt");
+		}
+		catch (FileNotFoundException ex) {
+			ex.printStackTrace();
+		}
+		catch (IOException ex) {
+			ex.printStackTrace();
+		}
+		finally {
+			// Close the BufferedWriter
+			try {
+				if (bw != null) {
+					bw.flush();
+					bw.close();
+				}
+			}
+			catch (IOException ex) {
+				ex.printStackTrace();
+			}
+		}
+	}
 
+	public static void main(String[] args) {
+		JSONLogger logger = new JSONLogger();
+		logger.logBrowserValue("firefox");
+		logger.logUserValue("user");
+		logger.logResultValue("result");
+		logger.logStartValue("start");
+		logger.logEndValue("end");
+		System.out.println(logger.getLogAsJSON());
 
-        int ch;
-        StringBuffer strContent = new StringBuffer("");
-        FileInputStream fin = null;
+		logger.logClickedObjects("id1", "now", "answer1");
+		System.out.println(logger.getLogAsJSON());
 
-        try {
-            fin = new FileInputStream("TESTI.txt");
+		logger.logClickedObjects("id2", "nowlater", "answer2");
+		System.out.println(logger.getLogAsJSON());
 
-            while ((ch = fin.read()) != -1) {
-                strContent.append((char) ch);
-            }
+		logger.writeJSONToFile("TESTI.txt");
 
-            fin.close();
+		int ch;
+		StringBuffer strContent = new StringBuffer("");
+		FileInputStream fin = null;
 
-        } catch (FileNotFoundException e) {
-        } catch (IOException ioe) {
-        }
+		try {
+			fin = new FileInputStream("TESTI.txt");
 
-        JSONParser parser = new JSONParser();
-        ContainerFactory containerFactory = new ContainerFactory() {
+			while ((ch = fin.read()) != -1) {
+				strContent.append((char) ch);
+			}
 
-            public List creatArrayContainer() {
-                return new LinkedList();
-            }
+			fin.close();
 
-            public Map createObjectContainer() {
-                return new LinkedHashMap();
-            }
-        };
+		}
+		catch (FileNotFoundException e) {
+		}
+		catch (IOException ioe) {
+		}
 
-        Map json = null;
-        try {
-            json = (Map) parser.parse(strContent.toString(), containerFactory);
-        } catch (ParseException pe) {
-        }
+		JSONParser parser = new JSONParser();
+		ContainerFactory containerFactory = new ContainerFactory() {
 
-        Iterator iter = json.entrySet().iterator();
-        while (iter.hasNext()) {
-            Map.Entry entry = (Map.Entry) iter.next();
-            System.out.println(entry.getKey() + "=>" + entry.getValue());
-        }
+			@Override
+			public List creatArrayContainer() {
+				return new LinkedList();
+			}
 
-        System.out.println(JSONValue.toJSONString(json));
+			@Override
+			public Map createObjectContainer() {
+				return new LinkedHashMap();
+			}
+		};
 
-    }
+		Map json = null;
+		try {
+			json = (Map) parser.parse(strContent.toString(), containerFactory);
+		}
+		catch (ParseException pe) {
+		}
+
+		Iterator iter = json.entrySet().iterator();
+		while (iter.hasNext()) {
+			Map.Entry entry = (Map.Entry) iter.next();
+			System.out.println(entry.getKey() + "=>" + entry.getValue());
+		}
+
+		System.out.println(JSONValue.toJSONString(json));
+
+	}
 }
