9f11b1a0dc67d39b117aa601181321fd94d85e6d
Reinhard Hatko <hatko@d3web.svn.invalid>
2012-03-07 22:08:00 +0000

    first version instant edit (still alpha)

---
+++ Research/KnowWE-Plugin-TestcaseTable-Extensions/src/main/java/de/d3web/we/testcase/TestcaseTableEditAction.java
@@ -0,0 +1,174 @@
+/*
+ * Copyright (C) 2012 University Wuerzburg, Computer Science VI
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package de.d3web.we.testcase;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
+import de.d3web.core.knowledge.KnowledgeBase;
+import de.d3web.core.knowledge.terminology.Choice;
+import de.d3web.core.knowledge.terminology.Question;
+import de.d3web.core.knowledge.terminology.QuestionChoice;
+import de.d3web.core.knowledge.terminology.QuestionDate;
+import de.d3web.core.knowledge.terminology.QuestionMC;
+import de.d3web.core.knowledge.terminology.QuestionNum;
+import de.d3web.core.knowledge.terminology.QuestionOC;
+import de.d3web.core.knowledge.terminology.QuestionText;
+import de.d3web.we.utils.D3webUtils;
+import de.knowwe.core.KnowWEArticleManager;
+import de.knowwe.core.KnowWEEnvironment;
+import de.knowwe.core.action.AbstractAction;
+import de.knowwe.core.action.UserActionContext;
+import de.knowwe.core.compile.packaging.KnowWEPackageManager;
+import de.knowwe.core.kdom.KnowWEArticle;
+import de.knowwe.core.kdom.parsing.Section;
+import de.knowwe.core.kdom.parsing.Sections;
+import de.knowwe.core.user.UserContext;
+import de.knowwe.kdom.defaultMarkup.DefaultMarkupType;
+import de.knowwe.testcases.table.TestcaseTableType;
+
+
+/**
+ * 
+ * @author Alexander Strehler
+ * @created 29.01.2012
+ */
+public class TestcaseTableEditAction extends AbstractAction {
+
+	/**
+	 * Beispielstring [ {'name': 'FrageNum','type': 'num'}, {'name':
+	 * 'FrageOC','type': 'oc','alternatives': ['a1','a2','a3','a4','a5']},
+	 * {'name': 'FrageYN','type': 'oc','alternatives': ['Yes','No']} ]
+	 */
+	@Override
+	public void execute(UserActionContext context) throws IOException {
+
+
+		// KnowledgeBase kb = D3webUtils.getKnowledgeBase(context.getWeb(),
+		// context.getTitle());
+		// List<Question> questions = new
+		// ArrayList<Question>(kb.getManager().getQuestions());
+
+		String sectionID = context.getParameter("kdomid");
+		Section<?> tablecontent = Sections.getSection(sectionID);
+
+		Section<TestcaseTableType> section = Sections.findAncestorOfType(tablecontent,
+				TestcaseTableType.class);
+
+		List<Question> questions = getQuestions(section, context);
+
+		Collections.sort(questions, new Comparator<Question>() {
+
+			@Override
+			public int compare(Question o1, Question o2) {
+				return o1.getName().toLowerCase().compareTo(o2.getName().toLowerCase());
+			}
+		});
+
+		StringBuilder bui = new StringBuilder();
+		bui.append("[");
+		for (Iterator<Question> it = questions.iterator(); it.hasNext(); it.next()) {
+			
+			Question q = it.next();
+			if (q.getName().toLowerCase().equals("start")
+					|| q.getName().toLowerCase().equals("now")) {
+				continue;
+			}
+
+			bui.append("{");
+			bui.append("'name': " + "'" + q.getName() + "'");
+			bui.append(",");
+			bui.append("'type': ");
+
+			if (q instanceof QuestionNum) {
+				bui.append("'num'");
+			}
+			else if (q instanceof QuestionOC) {
+				bui.append("'oc'");
+			}
+			else if (q instanceof QuestionMC) {
+				bui.append("'mc'");
+			}
+			else if (q instanceof QuestionDate) {
+				bui.append("'date'");
+			}
+			else if (q instanceof QuestionText) {
+				bui.append("'text'");
+			}
+
+			if (q instanceof QuestionChoice) {
+				bui.append(",");
+				bui.append("'alternatives': ");
+				bui.append("[");
+				List<Choice> answers = ((QuestionChoice) q).getAllAlternatives();
+				for (Choice choice : answers) {
+					bui.append("'" + choice.getName() + "'");
+					bui.append(",");
+				}
+				bui.replace(bui.length() - 1, bui.length(), "");
+				bui.append("]");
+			}
+
+			bui.append("}");
+			// if (it.hasNext())
+			bui.append(",");
+		}
+		bui.replace(bui.length() - 1, bui.length(), "");
+
+		bui.append("]");
+
+		context.setContentType("application/json");
+		context.getWriter().write(bui.toString());
+	}
+
+	private static List<Question> getQuestions(Section<TestcaseTableType> section, UserContext user) {
+		Collection<Question> questions = new HashSet<Question>();
+		String[] kbpackages = getPackages(section);
+		KnowWEEnvironment env = KnowWEEnvironment.getInstance();
+		KnowWEPackageManager packageManager = env.getPackageManager(section.getWeb());
+		KnowWEArticleManager articleManager = env.getArticleManager(user.getWeb());
+		for (String kbpackage : kbpackages) {
+			Set<String> articlesReferringTo = packageManager.getArticlesReferringTo(kbpackage);
+			for (String masterTitle : articlesReferringTo) {
+				KnowWEArticle masterArticle = articleManager.getArticle(masterTitle);
+				KnowledgeBase kb = D3webUtils.getKnowledgeBase(masterArticle.getWeb(),
+						masterArticle.getTitle());
+				questions.addAll(kb.getManager().getQuestions());
+			}
+		}
+		return new ArrayList<Question>(questions);
+	}
+
+	private static String[] getPackages(Section<TestcaseTableType> section) {
+		String[] kbpackages = DefaultMarkupType.getAnnotations(section,
+				KnowWEPackageManager.PACKAGE_ATTRIBUTE_NAME);
+		if (kbpackages.length == 0) {
+			kbpackages = new String[] { KnowWEPackageManager.DEFAULT_PACKAGE };
+		}
+		return kbpackages;
+	}
+
+}
---
+++ Research/KnowWE-Plugin-TestcaseTable-Extensions/src/main/resources/webapp/KnowWEExtension/scripts/TestcaseTableEditTools.js
@@ -0,0 +1,525 @@
+/**
+ * The KNOWWE global namespace object. If KNOWWE is already defined, the
+ * existing KNOWWE object will not be overwritten so that defined namespaces are
+ * preserved.
+ */
+if (typeof KNOWWE == "undefined" || !KNOWWE) {
+    var KNOWWE = {};
+}
+
+/**
+ * The KNOWWE.core global namespace object. If KNOWWE.core is already defined,
+ * the existing KNOWWE.core object will not be overwritten so that defined
+ * namespaces are preserved.
+ */
+if (typeof KNOWWE.plugin == "undefined" || !KNOWWE.plugin) {
+    KNOWWE.plugin = {};
+}
+
+if (typeof KNOWWE.plugin.testcasetable == "undefined" || !KNOWWE.plugin.testcasetable) {
+    KNOWWE.plugin.testcasetable = {};
+}
+
+
+/**
+ * This is the default edit provider.
+ * It renders every table cell in a text field.
+ * 
+ */
+KNOWWE.plugin.testcasetable.editProvider = (function(){
+
+
+	var objects;
+	var tableIndices = {};
+	var editId;
+		
+	return {
+		
+		/**
+		* fetches a Json Object which contains all questions and alternatives
+		* [{'name': 'FrageNum','type': 'num'}, {'name': 'FrageOC','type': 'oc','alternatives': ['a1','a2','a3','a4','a5']}, {'name': 'FrageYN','type': 'oc','alternatives': ['Yes','No']}]
+		*/
+		prepare: function(id){
+			editId = "defaultEditTable" + id;
+			
+			params = {
+				action : 'TestcaseTableEditAction',
+				KWikiWeb : 'default_web',
+				kdomid: id
+			};
+
+			// options for AJAX request
+			options = {
+				async: false,
+				url : KNOWWE.core.util.getURL( params ),
+				response : {
+					action : 'none',
+					fn : function(){
+						objects = Json.evaluate(this.responseText);
+					}
+				}
+			};
+
+			// send AJAX request
+			new _KA( options ).send(); 
+		},
+		
+		/**
+		* checks if the parameter value is a valid number
+		* Examples for valid numbers (without the ''):
+		* '-1', '1', '1.2', '-1.2', '1,2', '-1,2'
+		*/
+		isValidNum: function(value) {
+			return value.match(/[-+]?\b[0-9]+([.,][0-9]+)?$/);
+		},
+		
+		/**
+		* checks if the parameter value is a valid KnowWE/JSP-Date
+		*/
+		isValidDate: function(value) {
+			//ToDo noch aktualisieren
+			return value.match(/([0-3][0-9])[- /.]([0-1]?[0-9])[- /.](19|20)[0-9]{2}/);
+		},
+		
+		/**
+		* checks if the parameter value is valid text
+		* everything is valid text --> returns always true
+		*/
+		isValidText: function(value) {
+			//there are no restrictions for texts to be valid
+			return true;
+		},		
+		
+		/**
+		* checks for the HTML-Object el if its value is a valid value
+		* switches with the help of the name-tag which isValidxxx method is called
+		*/
+		isValid: function(el) {
+			var valid = false;
+			switch (el.name) {
+				default:
+				case "text":
+					valid = KNOWWE.plugin.testcasetable.editProvider.isValidText(el.value);
+					break;
+				case "num":
+					valid = KNOWWE.plugin.testcasetable.editProvider.isValidNum(el.value);
+					break;
+				case "date":
+					valid = KNOWWE.plugin.testcasetable.editProvider.isValidDate(el.value);
+					break;
+				case "oc":
+					valid = !el.options[el.selectedIndex].disabled;
+					break;
+			}
+			//HTML 5 attribut "classList"
+			if (valid) {
+				if (el.classList.contains("errorField")) {
+					el.classList.remove("errorField");
+				}
+			} else {
+				if (!el.classList.contains("errorField")) {
+					el.classList.add("errorField");
+				}
+			}
+		},
+		
+		/**
+		 * this method returns an HTML-String, that renders a DropDownBox (select)
+		 * arr: an Array with the DropDownBox values
+		 * value: the value which should be selected by default
+		 * onChange: the onChange Event, if undefined, no onChange-Event is registered
+		 */
+		renderOC: function(arr, value, onChange) {
+			var obj = {};
+			var val = false;
+			var inner = "";
+		
+			var box = '<select name="oc" size="1"';
+			
+			if (onChange != undefined || onChange == "") inner += ' onChange="' + onChange + '"';
+			
+			inner += '>';
+			
+			for (var i = 0; i < arr.length; i++) {
+				inner += '<option';
+					if (value != undefined && arr[i].toLowerCase() == value.toLowerCase()) {
+						inner += ' selected="selected"';
+						val = true;
+					}
+				inner += '>' + arr[i] + '</option>';
+			}
+			if (!val && value != undefined) {
+				box += ' class="errorField"';
+				inner += '<option disabled="disabled" selected="selected">' + value + '</option>';
+			}
+			inner += '</select>';
+			
+			obj.html = box + inner;
+			obj.valid = val;
+			
+			return obj;
+			
+		},
+		
+		/**
+		 * this method returns an HTML-String, that renders an Input-Textfield
+		 * value: default value of the Input-Box, if value == undefined no value is set
+		 */
+		renderNum: function(value) {
+			var obj = {};
+			var end = "";
+			var val = false;
+		
+			var box = '<input name="num" type="text" size="10"';
+			if (value != undefined) {
+				end += ' value="' + value + '"';
+				val = KNOWWE.plugin.testcasetable.editProvider.isValidNum(value);
+			}
+			end += '>';
+			
+			if (!val) {
+				box += ' class="errorField"';
+			}
+			
+			obj.html = box + end;
+			obj.valid = val;
+			
+			return obj;
+		},
+		
+		/**
+		 * this method returns an HTML-String, that renders an Input-Textfield
+		 * value: default value of the Input-Box, if value == undefined no value is set
+		 */
+		renderText: function(value) {
+			var obj = {};
+			var end = "";
+			var val = false;
+		
+			var box = '<input name="text" type="text" size="10"';
+			if (value != undefined) {
+				end += ' value="' + value + '"';
+				val = KNOWWE.plugin.testcasetable.editProvider.isValidText(value);
+			}
+			end += '>';
+			
+			if (!val) {
+				box += ' class="errorField"';
+			}
+			
+			obj.html = box + end;
+			obj.valid = val;
+			
+			return obj;
+		},
+		
+		/**
+		 * this method returns an HTML-String, that renders an Input-Textfield
+		 * value: default value of the Input-Box, if value == undefined no value is set
+		 */
+		renderDate: function(value) {
+			var obj = {};
+			var end = "";
+			var val = false;
+		
+			var box = '<input name="date" type="text" size="10"';
+			if (value != undefined) {
+				end += ' value="' + value + '"';
+				val = KNOWWE.plugin.testcasetable.editProvider.isValidDate(value);
+			}
+			end += '>';
+			
+			if (!val) {
+				box += ' class="errorField"';
+			}
+			
+			obj.html = box + end;
+			obj.valid = val;
+			
+			return obj;
+		},
+		
+		/**
+		* will be executed after the table is put in the DOM-structure
+		*/
+		postProcess: function(id) {
+			var tmp = document.getElementById(editId).childNodes;
+			while (true) {
+				if (tmp[0].tagName == "TR") {
+					break;
+				}
+				tmp = tmp[0].childNodes;
+			}
+			
+			//in a first step just textboxes are rendered; textboxes will be replaced with specific textboxes/Dropdownboxes
+			var firstTr = tmp[0].childNodes;
+			for (var i = 2; i < firstTr.length; i++) {
+				KNOWWE.plugin.testcasetable.editProvider.rerenderColumn(i);
+			}
+			
+			//adding Changelistener to the first row and all other fields
+			for (var j = 0; j < tmp[0].childNodes.length; j++) {
+				KNOWWE.plugin.testcasetable.editProvider.addChangeColHandler(j);
+				KNOWWE.plugin.testcasetable.editProvider.addValidHandler(j);
+			}
+		},
+		
+		/**
+		* adding changeListener to the first row in the column specified with the int parameter col
+		*/
+		addChangeColHandler: function(col) {
+			var tmp = document.getElementById(editId).childNodes;
+			while (true) {
+				if (tmp[0].tagName == "TR") {
+					break;
+				}
+				tmp = tmp[0].childNodes;
+			}
+			tmp[0].childNodes[col].firstChild.onchange = function(){KNOWWE.plugin.testcasetable.editProvider.changeColumn(this)};
+		},
+		
+		/**
+		* adding changeListener to all rows in the column specified with the int parameter col
+		*/
+		addValidHandler: function(col) {
+			var tmp = document.getElementById(editId).childNodes;
+			while (true) {
+				if (tmp[0].tagName == "TR") {
+					break;
+				}
+				tmp = tmp[0].childNodes;
+			}
+			for (var i = 1; i < tmp.length; i++) {
+				tmp[i].childNodes[col].firstChild.onchange = function(){KNOWWE.plugin.testcasetable.editProvider.isValid(this)};
+			}
+		},
+		
+		/**
+		* is executed if a Dropdownbox in the first row is changed
+		*/
+		changeColumn: function(el) {
+			if (el.classList.contains("errorField")) {
+				el.classList.remove("errorField");
+			}
+			var col = KNOWWE.table.edit.getPositionInTable(el).column;
+			KNOWWE.plugin.testcasetable.editProvider.rerenderColumn(col);
+		},
+		
+		/**
+		 * Called after inserting a new column into the table
+		 * colNr : index of new column
+		 */
+		columnAdded : function(colNr){
+			KNOWWE.plugin.testcasetable.editProvider.addChangeColHandler(colNr);
+		},
+		
+		/**
+		 * Called after inserting a new row into the table
+		 * rowNr : index of new row
+		 */
+		rowAdded : function(rowNr){
+			
+		},
+		
+		/**
+		* replaces all input fields of a column with new fields
+		* copies the old value into the new input fields
+		*/
+		rerenderColumn: function(colNr) {
+			var tmp = document.getElementById(editId).childNodes;
+			while (true) {
+				if (tmp[0].tagName == "TR") {
+					break;
+				}
+				tmp = tmp[0].childNodes;
+			}
+			var oldInput;
+			var value;
+			var tmpInnerHtml, newHtml;
+			for (var i = 1; i < tmp.length; i++) {
+				//get old value, remove the input-box, save the old inner-HTML
+				oldInput = tmp[i].childNodes[colNr].firstChild;
+				value = KNOWWE.plugin.testcasetable.editProvider.getValue(oldInput, i, colNr);
+				tmp[i].childNodes[colNr].removeChild(oldInput);
+				tmpInnerHtml = tmp[i].childNodes[colNr].innerHTML;
+				
+				//create new input-box, assign new inner-HTML string
+				newHtml = KNOWWE.plugin.testcasetable.editProvider.getDisplayComponent(i, colNr, value);
+				tmp[i].childNodes[colNr].innerHTML = newHtml + tmpInnerHtml;
+			}
+			KNOWWE.plugin.testcasetable.editProvider.addValidHandler(colNr);
+		},
+		
+		/**
+		* returns the questiontype of the column specified in the in parameter col
+		* return the type specified in the JSON-Object
+		*/
+		getQuestionType : function(col) {
+			if (document.getElementById(editId) == null) {
+				return undefined;
+			}
+			var obj = {};
+			var tmp = document.getElementById(editId).childNodes;
+			while (true) {
+				if (tmp[0].tagName == "TR") {
+					break;
+				}
+				tmp = tmp[0].childNodes;
+			}
+			var rightNode = tmp[0].childNodes[col];
+			var el = rightNode.firstChild;
+			var index = el.selectedIndex;			
+			var name = el.options[index].value.toLowerCase();
+			
+			var objIndex = -1;			
+			if (index >= objects.length) {
+				obj.type = "text";
+				return obj;
+			}
+			
+			if (objects[index].name.toLowerCase() == name) {
+				objIndex = index;
+			} else {
+				for (var i = 0; i < objects.length; i++) {
+					if (objects[i].name.toLowerCase() == name) {
+						objIndex = i;
+						break;
+					}
+				}
+			}
+			
+			obj.type = objects[objIndex].type;
+			
+			if (obj.type == "oc") {
+				obj.alternatives = objects[objIndex].alternatives;
+			}
+			
+			return obj;
+		},
+	
+		/**
+		 * this method must return an HTML-String, that renders a component at the specified location
+		 * (row, col) and displays the supplied value. 
+		 * for non-editable values it can return a <span> or something similar.
+		 * row / col: index in the table
+		 * value: value to display, of undefined, if no value should be displayed
+		 * 
+		 */
+		getDisplayComponent : function(row, col, value){
+			var obj;
+	
+			//default: Input-Textfield
+			var result = KNOWWE.plugin.testcasetable.editProvider.renderText(value).html;
+			
+			if (objects != undefined) {
+				if (row == 0) {
+					//first Row-->just Spans and Dropdownboxes
+					if (col > 1) {
+						var arr = new Array(objects.length);
+						for (var i = 0; i < objects.length; i++) {
+							arr[i] = objects[i].name;
+						}
+						obj = KNOWWE.plugin.testcasetable.editProvider.renderOC(arr, value);
+						result = obj.html;
+					} else if (col <= 1) {
+						result = '<span>' + value + '</span>';
+					}
+				} else {
+					//col <= 1 musss nicht ber�cksichtigt werden, da in Spalte 0&1 immer nur InputFelder stehen
+					if (col > 1) {
+						var qtype = KNOWWE.plugin.testcasetable.editProvider.getQuestionType(col);
+						if (qtype == undefined) {
+							return result;
+						}
+						switch (qtype.type) {
+							default:
+								break;
+							case "date":
+								result = KNOWWE.plugin.testcasetable.editProvider.renderDate(value).html;
+								break;				
+							case "text": 
+								result = KNOWWE.plugin.testcasetable.editProvider.renderText(value).html;
+								break;
+							case "num":
+								result = KNOWWE.plugin.testcasetable.editProvider.renderNum(value).html;
+								break;					
+							case "oc":
+								result = KNOWWE.plugin.testcasetable.editProvider.renderOC(qtype.alternatives, value).html;
+								break;
+						}
+					}
+				}
+			}
+			
+			return result;
+		},
+		
+		/**
+		 * This method returns the default value for newly created cells, 
+		 * after inserting a row or column. This method can also return 'undefined', 
+		 * e.g. to leave a textual input empty 
+		 */
+		getDefaultValue : function(row, col) {
+			if (row <= 1) {
+				return "";
+			} else {
+				return KNOWWE.plugin.testcasetable.editProvider.getValue(undefined, (row-1), col);
+			}
+		},
+		
+		/**
+		 * This method returns the value that has been entered at the supplied position in the table, as a string. 
+		 * The returned string representation is written into the article.
+		 * As a convenience, the first child of the cell at that position is supplied
+		 * row/col: position in the table of the cell
+		 * el: the first child of the td/th at (row, col)
+		 */
+		getValue : function(el, row, col) {
+			if (el != undefined) {
+				if (el.tagName.toLowerCase() == "span") {
+					return el.innerHTML;
+				} else if (el.tagName.toLowerCase() == "select") {
+					return el.options[el.selectedIndex].value;
+				} else if (el.tagName.toLowerCase() == "input") {
+					return el.value;
+				}
+				return el.value;
+			} else {
+				var tmp = document.getElementById(editId).childNodes;
+				while (true) {
+					if (tmp[0].tagName == "TR") {
+						break;
+					}
+					tmp = tmp[0].childNodes;
+				}
+				return KNOWWE.plugin.testcasetable.editProvider.getValue(tmp[row].childNodes[col].firstChild);
+			}
+		},
+		
+		/**
+		 * Returns an array of actions, that are available for the cell at the specified coordinates.
+		 * 
+		 */
+		getActions : function(el, row, col, editor) {
+			var actions = [];
+		
+			if (row == 0 && col > 1){
+				actions.push({name: 'Delete column', f: (function(){editor.delCol(el)}) });
+				actions.push({name: 'Add column left', f: (function(){editor.addCol(el, 0)})});
+				actions.push({name: 'Add column right', f: (function(){editor.addCol(el, 1)})});
+			} else if (row == 0 && col == 1){
+				actions.push({name: 'Add column right', f: (function(){editor.addCol(el, 1)})});
+			}
+			
+			if (col == 0 && row > 0){
+				actions.push({name: 'Delete row', f: (function(){editor.delRow(el)})});
+				actions.push({name: 'Add row above', f: (function(){editor.addRow(el, 0)})});
+				actions.push({name: 'Add row below', f: (function(){editor.addRow(el, 1)})});
+			} else if (col == 0 && row == 0){
+				actions.push({name: 'Add row below', f: (function(){editor.addRow(el, 1)})});
+			}
+			
+			return actions;
+		}
+	}
+})();
