535e52ef99d00e9b4e516e3b104c0b6b45a2ccab
Johannes Dienst <dienst86@d3web.svn.invalid>
2012-03-22 07:25:30 +0000

    * Moved Toolprovider

--- Research/KnowWE-Plugin-UserSupportNew/src/main/java/de/knowwe/usersupport/instantedit/UserSupportInstantEditToolProvider.java
+++ Research/KnowWE-Plugin-UserSupportNew/src/main/java/de/knowwe/usersupport/instantedit/UserSupportInstantEditToolProvider.java
@@ -16,7 +16,7 @@
  * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA, or see the FSF
  * site: http://www.fsf.org.
  */
-package de.d3web.we.instantedit;
+package de.knowwe.usersupport.instantedit;
 
 import de.knowwe.core.kdom.parsing.Section;
 import de.knowwe.core.user.UserContext;
