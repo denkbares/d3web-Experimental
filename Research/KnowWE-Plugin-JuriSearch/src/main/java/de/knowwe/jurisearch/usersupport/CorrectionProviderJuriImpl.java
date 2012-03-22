5071e625d7b9179613bf9b61c4dfe711cafd73c0
Sebastian Furth <sebastian.furth@denkbares.com>
2012-03-22 09:26:39 +0000

    fixed wrong import

--- Research/KnowWE-Plugin-JuriSearch/src/main/java/de/knowwe/jurisearch/usersupport/CorrectionProviderJuriImpl.java
+++ Research/KnowWE-Plugin-JuriSearch/src/main/java/de/knowwe/jurisearch/usersupport/CorrectionProviderJuriImpl.java
@@ -1,20 +1,20 @@
 /*
  * Copyright (C) 2012 University Wuerzburg, Computer Science VI
- *
- * This is free software; you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License as
- * published by the Free Software Foundation; either version 3 of
- * the License, or (at your option) any later version.
- *
- * This software is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this software; if not, write to the Free
- * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
- * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ * 
+ * This is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 3 of the License, or (at your option) any
+ * later version.
+ * 
+ * This software is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this software; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA, or see the FSF
+ * site: http://www.fsf.org.
  */
 package de.knowwe.jurisearch.usersupport;
 
@@ -24,11 +24,10 @@ import java.util.List;
 
 import de.d3web.we.algorithm.DialogComponent;
 import de.d3web.we.algorithm.Suggestion;
-import de.d3web.we.correction.ApproximateCorrectionProvider;
 import de.knowwe.core.kdom.Article;
 import de.knowwe.core.kdom.parsing.Section;
+import de.knowwe.usersupport.correction.ApproximateCorrectionProvider;
 import de.knowwe.usersupport.util.UserSupportUtil;
-
 
 /**
  * 
@@ -51,7 +50,7 @@ public class CorrectionProviderJuriImpl implements ApproximateCorrectionProvider
 
 		List<Suggestion> suggestions =
 				DialogComponent.getInstance().
-				getBestSuggestionsAllAlgorithms(toMatch, localStrings);
+						getBestSuggestionsAllAlgorithms(toMatch, localStrings);
 
 		return suggestions;
 	}
