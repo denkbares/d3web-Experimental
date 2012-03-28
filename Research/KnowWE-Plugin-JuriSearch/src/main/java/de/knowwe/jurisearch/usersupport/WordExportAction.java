da238cd4678572e2e05d49777d392f9833ef54c1
Johannes Dienst <dienst86@d3web.svn.invalid>
2012-03-28 10:14:54 +0000

    * KnowWEExtensionPath used for Export

--- Research/KnowWE-Plugin-JuriSearch/src/main/java/de/knowwe/jurisearch/usersupport/WordExportAction.java
+++ Research/KnowWE-Plugin-JuriSearch/src/main/java/de/knowwe/jurisearch/usersupport/WordExportAction.java
@@ -1,20 +1,20 @@
 /*
  * Copyright (C) 2012 University Wuerzburg, Computer Science VI
- *
- * This is free software; you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License as
- * published by the Free Software Foundation; either version 3 of
- * the License, or (at your option) any later version.
- *
- * This software is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this software; if not, write to the Free
- * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
- * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ * 
+ * This is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 3 of the License, or (at your option) any
+ * later version.
+ * 
+ * This software is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this software; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA, or see the FSF
+ * site: http://www.fsf.org.
  */
 package de.knowwe.jurisearch.usersupport;
 
@@ -28,7 +28,7 @@ import de.knowwe.core.Environment;
 import de.knowwe.core.action.AbstractAction;
 import de.knowwe.core.action.UserActionContext;
 import de.knowwe.core.kdom.Article;
-
+import de.knowwe.usersupport.poi.PoiUtils;
 
 /**
  * 
@@ -49,7 +49,8 @@ public class WordExportAction extends AbstractAction
 		Article article =
 				Environment.getInstance().getArticle(context.getWeb(), context.getTitle());
 
-		String extensionPath = System.getProperty("java.io.tmpdir") + "/docbook-" + title + ".docx";
+		String extensionPath = Environment.getInstance().getKnowWEExtensionPath() + "/docbook-"
+				+ title + "-" + PoiUtils.getIdPostfix() + ".docx";
 		File file = new File(extensionPath);
 		FileOutputStream out = new FileOutputStream(file);
 
@@ -63,8 +64,8 @@ public class WordExportAction extends AbstractAction
 		// write the downloadlink beneath the exportbutton
 		Writer writer = context.getWriter();
 		writer.append(
-				"<a href=\"file://"+ extensionPath + "\" target=\"_blank\">" +
-						bundle.getString("download-export") +  "</a>"
+				"<a href=\"file://" + extensionPath + "\" target=\"_blank\">" +
+						bundle.getString("download-export") + "</a>"
 				);
 	}
 }
