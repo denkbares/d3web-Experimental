7cb7854bdcff83d144769978ec4e35601de2fad2
Volker Belli <volker.belli@denkbares.com>
2012-05-03 17:50:15 +0000

    Removed a very few research-warnings

--- Research/KnowWE-Plugin-OneQuestionDialog/src/main/java/de/d3web/we/oqd/OneQuestionDialogAction.java
+++ Research/KnowWE-Plugin-OneQuestionDialog/src/main/java/de/d3web/we/oqd/OneQuestionDialogAction.java
@@ -1,16 +1,16 @@
 /*
  * Copyright (C) 2010 University Wuerzburg, Computer Science VI
- *
+ * 
  * This is free software; you can redistribute it and/or modify it under the
  * terms of the GNU Lesser General Public License as published by the Free
  * Software Foundation; either version 3 of the License, or (at your option) any
  * later version.
- *
+ * 
  * This software is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
  * details.
- *
+ * 
  * You should have received a copy of the GNU Lesser General Public License
  * along with this software; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA, or see the FSF
@@ -27,7 +27,7 @@ import de.knowwe.core.action.AbstractAction;
 import de.knowwe.core.action.UserActionContext;
 
 /**
- *
+ * 
  * @author Florian Ziegler
  * @created 17.08.2010
  */
@@ -38,7 +38,7 @@ public class OneQuestionDialogAction extends AbstractAction {
 
 		Map<String, String> map = context.getParameters();
 		String web = context.getWeb();
-		String topic = context.getTopic();
+		String topic = context.getTitle();
 		String type = map.get("type");
 		String question = map.get("question");
 		String questionId = map.get("questionId");
