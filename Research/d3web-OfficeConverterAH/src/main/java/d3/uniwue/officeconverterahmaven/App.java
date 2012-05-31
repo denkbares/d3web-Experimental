9f192c6818d18c5d9d1d8fda133aeb950445a08d
Elmar Boehler <boehler@d3web.svn.invalid>
2012-05-31 07:16:07 +0000

    Fixes due to the changes in the OfficeConverter

--- Research/d3web-OfficeConverterAH/src/main/java/d3/uniwue/officeconverterahmaven/App.java
+++ Research/d3web-OfficeConverterAH/src/main/java/d3/uniwue/officeconverterahmaven/App.java
@@ -36,7 +36,10 @@ public class App {
 
 	public static void main(String[] args) {
 		try {
-			Config conf = new Config(FILETYPE_OUTPUT, false, "UTF-8", embeddedObjectsDir , true, false, true);
+			
+			Config conf = new Config().setEmbeddedObjectsDir(embeddedObjectsDir).setWithLists(true).setLeaveUmlauts(true).setWithHeadlineDepths(true);
+			
+			//Config conf = new Config(FILETYPE_OUTPUT, false, "UTF-8", embeddedObjectsDir , true, false, true);
 			Converter.convertFile2File(new File(wordinput), 
 					new File(cleanedIntermedHtml), conf);
 			String s = StringUtils.readFileString(cleanedIntermedHtml);
--- Research/d3web-OfficeConverterAH/src/main/java/d3/uniwue/officeconverterahmaven/Test.java
+++ Research/d3web-OfficeConverterAH/src/main/java/d3/uniwue/officeconverterahmaven/Test.java
@@ -33,7 +33,8 @@ public class Test {
 
 	public static void main(String[] args) {
 		try {
-			Config conf = new Config(FILETYPE_OUTPUT, false, "UTF-8", embeddedObjectsDir , true, false, true);
+			Config conf = new Config().setEmbeddedObjectsDir(embeddedObjectsDir).setWithLists(true).setLeaveUmlauts(true).setWithHeadlineDepths(true);
+			//Config conf = new Config(FILETYPE_OUTPUT, false, "UTF-8", embeddedObjectsDir , true, false, true);
 			Converter.convertFile2File(new File(wordinput), 
 					new File(cleanedIntermedHtml), conf);
 			String s = StringUtils.readFileString(cleanedIntermedHtml);
