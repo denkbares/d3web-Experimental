34cd6d8549e73fec05b5e7877fc9ae45af279e0c
Albrecht Striffler <albrecht.striffler@denkbares.com>
2012-07-20 09:18:29 +0000

    Fixed issue with ArticleLinkeNodeRenderer rendering article links to greedy.

--- Research/KnowWE-Plugin-Rdf2GoSemanticCore/src/main/java/de/knowwe/rdf2go/sparql/ArticleLinkNodeRenderer.java
+++ Research/KnowWE-Plugin-Rdf2GoSemanticCore/src/main/java/de/knowwe/rdf2go/sparql/ArticleLinkNodeRenderer.java
@@ -3,6 +3,7 @@ package de.knowwe.rdf2go.sparql;
 import de.knowwe.core.Environment;
 import de.knowwe.core.utils.KnowWEUtils;
 import de.knowwe.core.utils.Strings;
+import de.knowwe.rdf2go.Rdf2GoCore;
 import de.knowwe.rdf2go.utils.Rdf2GoUtils;
 
 public class ArticleLinkNodeRenderer implements SparqlResultNodeRenderer {
@@ -10,10 +11,23 @@ public class ArticleLinkNodeRenderer implements SparqlResultNodeRenderer {
 	@Override
 	public String renderNode(String text, String variable) {
 		boolean foundArticle = false;
-		String[] statements = text.split(" ");
+		String lns = Rdf2GoCore.getInstance().getLocalNamespace();
+
+		String[] statements;
+		// We are only interested in statements from the local name space.
+		// Other name spaces or no name space (simple string) probably is not
+		// representing an article in the wiki.
+		if (text.startsWith(lns)) {
+			statements = text.split(" ");
+		}
+		else {
+			return text;
+		}
+
 		String[] articleLinks = new String[statements.length];
 		for (int i = 0; i < statements.length; i++) {
 			String statement = statements[i];
+			statement = Strings.decodeURL(statement);
 			if (statement.isEmpty()) continue;
 			String title = Rdf2GoUtils.trimNamespace(statement);
 			if (Environment.getInstance().getWikiConnector().doesArticleExist(title)) {
