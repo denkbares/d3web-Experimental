d2665a04569f5d76f94afd0ab866002266672a0c
Martina Freiberg <mfreiberg@d3web.svn.invalid>
2012-07-31 13:35:49 +0000

    Some more properties to be set for knowledge bases - required to model the specific itree mechanisms

--- Research/d3web-Plugin-ProKEtProperties/src/main/java/de/d3web/proket/d3web/properties/ProKEtProperties.java
+++ Research/d3web-Plugin-ProKEtProperties/src/main/java/de/d3web/proket/d3web/properties/ProKEtProperties.java
@@ -4,60 +4,49 @@ import de.d3web.core.knowledge.terminology.info.Property;
 
 public class ProKEtProperties {
 
-	public static final Property<String> DATE_FORMAT = Property.getProperty("date_format",
-			String.class);
-
-	public static final Property<String> TEXT_FORMAT = Property.getProperty("text_format",
-			String.class);
-
-	public static final Property<Boolean> LONG_TEXT = Property.getProperty("long_text",
-			Boolean.class);
-
-	public static final Property<String> POPUP = Property.getProperty("popup", String.class);
-
-	public static final Property<String> GRID = Property.getProperty("grid", String.class);
-
-	public static final Property<Boolean> SHOW_IN_HEADER = Property.getProperty("show_in_header",
-			Boolean.class);
-
-	public static final Property<Boolean> HIDE = Property.getProperty("hide", Boolean.class);
-
-	public static final Property<Integer> POSITION_IN_HEADER = Property.getProperty(
-			"position_in_header", Integer.class);
-
-	public static final Property<String> HEADER_TEXT = Property.getProperty(
-			"header_text", String.class);
-
-	public static final Property<String> IMAGE = Property.getProperty("image", String.class);
-
-	public static final Property<String> IMAGEWIDTH = Property.getProperty("imagewidth",
-			String.class);
-
-	public static final Property<String> IMAGEHEIGHT = Property.getProperty("imageheight",
-			String.class);
-
-	public static final Property<String> IMAGEMAP = Property.getProperty("imagemap", String.class);
-
-	public static final Property<String> DROPDOWN_MENU_OPTIONS = Property.getProperty(
-			"dropdown_menu_options", String.class);
-
-	public static final Property<String> DROPDOWN_MENU_DEFAULT = Property.getProperty(
-			"dropdown_menu_default", String.class);
-
-	public static final Property<Boolean> USE_AS_FILENAME = Property.getProperty(
-			"use_as_filename", Boolean.class);
-
-	public static final Property<String> BEFORE = Property.getProperty(
-			"before", String.class);
-
-	public static final Property<String> AFTER = Property.getProperty(
-			"after", String.class);
-        
-        public static final Property<Boolean> ITREESHOWN = Property.getProperty(
-			"itree_shown", Boolean.class);
-        
-        public static final Property<Boolean> ITREEINIT = Property.getProperty(
-			"itree_init", Boolean.class);
-        
-        
+    public static final Property<String> DATE_FORMAT = Property.getProperty("date_format",
+            String.class);
+    public static final Property<String> TEXT_FORMAT = Property.getProperty("text_format",
+            String.class);
+    public static final Property<Boolean> LONG_TEXT = Property.getProperty("long_text",
+            Boolean.class);
+    public static final Property<String> POPUP = Property.getProperty("popup", String.class);
+    public static final Property<String> GRID = Property.getProperty("grid", String.class);
+    public static final Property<Boolean> SHOW_IN_HEADER = Property.getProperty("show_in_header",
+            Boolean.class);
+    public static final Property<Boolean> HIDE = Property.getProperty("hide", Boolean.class);
+    public static final Property<Integer> POSITION_IN_HEADER = Property.getProperty(
+            "position_in_header", Integer.class);
+    public static final Property<String> HEADER_TEXT = Property.getProperty(
+            "header_text", String.class);
+    public static final Property<String> IMAGE = Property.getProperty("image", String.class);
+    public static final Property<String> IMAGEWIDTH = Property.getProperty("imagewidth",
+            String.class);
+    public static final Property<String> IMAGEHEIGHT = Property.getProperty("imageheight",
+            String.class);
+    public static final Property<String> IMAGEMAP = Property.getProperty("imagemap", String.class);
+    public static final Property<String> DROPDOWN_MENU_OPTIONS = Property.getProperty(
+            "dropdown_menu_options", String.class);
+    public static final Property<String> DROPDOWN_MENU_DEFAULT = Property.getProperty(
+            "dropdown_menu_default", String.class);
+    public static final Property<Boolean> USE_AS_FILENAME = Property.getProperty(
+            "use_as_filename", Boolean.class);
+    public static final Property<String> BEFORE = Property.getProperty(
+            "before", String.class);
+    public static final Property<String> AFTER = Property.getProperty(
+            "after", String.class);
+    public static final Property<Boolean> ITREEINIT = Property.getProperty(
+            "itreeinit", Boolean.class);
+    public static final Property<Boolean> NO_DEFINING = Property.getProperty(
+            "nodefining", Boolean.class);
+    public static final Property<Boolean> ORTYPE = Property.getProperty(
+            "ortype", Boolean.class);
+     public static final Property<Boolean> DUMMY = Property.getProperty(
+            "dummy", Boolean.class);
+    public static final Property<String> WEBLINK = Property.getProperty(
+            "weblink", String.class);
+    public static final Property<String> PDFLINK = Property.getProperty(
+            "pdflink", String.class);
+    public static final Property<String> RECHNERLINK = Property.getProperty(
+            "rechnerlink", String.class);
 }
