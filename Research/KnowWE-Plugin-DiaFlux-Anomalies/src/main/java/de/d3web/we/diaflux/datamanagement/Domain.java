6bc5d74612f1107b5b8db939e88d70a6f129a90e
Reinhard Hatko <hatko@d3web.svn.invalid>
2012-08-21 17:55:04 +0000

    Did not commit previously: - Reworked diaflux-anomalies detection (not yet finished)

--- Research/KnowWE-Plugin-DiaFlux-Anomalies/src/main/java/de/d3web/we/diaflux/datamanagement/Domain.java
+++ Research/KnowWE-Plugin-DiaFlux-Anomalies/src/main/java/de/d3web/we/diaflux/datamanagement/Domain.java
@@ -1,171 +1,43 @@
 /*
  * Copyright (C) 2012 University Wuerzburg, Computer Science VI
- * 
- * This is free software; you can redistribute it and/or modify it under the
- * terms of the GNU Lesser General Public License as published by the Free
- * Software Foundation; either version 3 of the License, or (at your option) any
- * later version.
- * 
- * This software is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
- * details.
- * 
- * You should have received a copy of the GNU Lesser General Public License
- * along with this software; if not, write to the Free Software Foundation,
- * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA, or see the FSF
- * site: http://www.fsf.org.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
  */
 package de.d3web.we.diaflux.datamanagement;
 
-import java.util.LinkedList;
-import java.util.List;
 
-public class Domain<T extends IValue<T>> {
 
-	private List<T> list;
+/**
+ * 
+ * @author Reinhard Hatko
+ * @created 21.06.2012
+ */
+public interface Domain {
 
-	public Domain() {
-		list = new LinkedList<T>();
-	}
 
-	public boolean add(T v) {
-		list.add(v);
-		mergeAll();
-		return true;
-	}
+	Domain add(Domain domain);
 
-	public boolean addAll(Domain<T> d) {
-		list.addAll(d.getList());
-		mergeAll();
-		return false;
-	}
+	Domain negate();
 
-	private void mergeAll() {
-		List<T> result = new LinkedList<T>();
-		result.addAll(this.list);
-		List<T> oldList = new LinkedList<T>();
-		while (!oldList.equals(result)) {
-			oldList = new LinkedList<T>();
-			oldList.addAll(result);
-			for (int i = 0; i < oldList.size(); i++) {
-				for (int j = i + 1; j < oldList.size(); j++) {
-					T t1 = oldList.get(i);
-					T t2 = oldList.get(j);
-					if (t1.intersects(t2)) {
-						result.remove(t1);
-						result.remove(t2);
-						result.add((T) t1.mergeWith(t2));
-					}
-				}
-			}
-		}
-		this.list = result;
-	}
+	boolean contains(Domain domain);
 
-	private void intersectAll() {
-		List<T> result = new LinkedList<T>();
-		result.addAll(this.list);
-		List<T> oldList = new LinkedList<T>();
-		while (!oldList.equals(result)) {
-			oldList = new LinkedList<T>();
-			oldList.addAll(result);
-			for (int i = 0; i < oldList.size(); i++) {
-				for (int j = i + 1; j < oldList.size(); j++) {
-					T t1 = oldList.get(i);
-					T t2 = oldList.get(j);
+	boolean intersects(Domain domain);
 
-					if (t1.intersects(t2)) {
-						result.remove(t1);
-						result.remove(t2);
-						result.add((T) t1.intersectWith(t2));
-					}
-				}
-			}
-		}
-		this.list = result;
-	}
+	boolean isEmpty();
 
-	public boolean intersects(Domain<T> d) {
-		for (T t1 : d.getList()) {
-			for (T t2 : list) {
-				if (t1.intersects(t2)) {
-					return true;
-				}
-			}
-		}
-		return false;
-	}
+	Domain intersect(Domain domain);
 
-	public boolean contains(Domain<T> d) {
-		boolean contained = false;
-		for (T nV : d.list) {
-			for (T nV2 : list) {
-				if (nV2.containsValue(nV)) contained = true;
-			}
-			if (!contained) return false;
-		}
-		return true;
-	}
-
-	public boolean isEmpty() {
-		return list.isEmpty();
-	}
-
-	public Domain<T> negate() {
-		List<T> newlist = new LinkedList<T>();
-		Domain<T> result = new Domain<T>();
-
-		for (T t1 : list) {
-			newlist.addAll(t1.negate());
-		}
-		for (T v : newlist) {
-			result.add(v);
-		}
-		result.mergeAll();
-		return result;
-	}
-
-	public List<T> getList() {
-		return list;
-	}
-
-	public Domain<T> substract(Domain<T> d) {
-		Domain<T> domain = new Domain<T>();
-		List<T> resultList = new LinkedList<T>();
-		for (T t : list) {
-			for (T d_t : d.list) {
-				for (T new_t : t.substract(d_t)) {
-					if (!new_t.isEmpty()) {
-						resultList.add(new_t);
-						domain.list.add(new_t);
-					}
-				}
-			}
-		}
-
-		domain.intersectAll();
-		return domain;
-	}
-
-	public Domain<T> intersectWith(Domain<T> d) {
-		if (!this.intersects(d)) return new Domain<T>();
-		Domain<T> result = new Domain<T>();
-		for (T t1 : list) {
-			for (T t2 : d.list) {
-				if (t1.intersects(t2)) result.add((T) t1.intersectWith(t2));
-			}
-		}
-		result.mergeAll();
-		return result;
-	}
-
-	@Override
-	public String toString() {
-		String result = "";
-		for (T t : list) {
-			result += t.toString() + " ";
-		}
-		return "Domain: " + result;
-	}
 }
--- Research/KnowWE-Plugin-DiaFlux-Anomalies/src/main/java/de/d3web/we/diaflux/datamanagement/EvalResult.java
+++ Research/KnowWE-Plugin-DiaFlux-Anomalies/src/main/java/de/d3web/we/diaflux/datamanagement/EvalResult.java
@@ -1,246 +1,148 @@
 /*
  * Copyright (C) 2012 University Wuerzburg, Computer Science VI
- * 
- * This is free software; you can redistribute it and/or modify it under the
- * terms of the GNU Lesser General Public License as published by the Free
- * Software Foundation; either version 3 of the License, or (at your option) any
- * later version.
- * 
- * This software is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
- * details.
- * 
- * You should have received a copy of the GNU Lesser General Public License
- * along with this software; if not, write to the Free Software Foundation,
- * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA, or see the FSF
- * site: http://www.fsf.org.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
  */
 package de.d3web.we.diaflux.datamanagement;
 
+import java.util.Collection;
 import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Set;
 
+import de.d3web.core.knowledge.TerminologyObject;
+
 /**
+ * This class represents the results of an evaluation of a condition or action.
  * 
- * @author Roland Jerg
- * @created 08.05.2012
+ * @author Reinhard Hatko
+ * @created 28.06.2012
  */
 public class EvalResult {
 
-	private final HashMap<String, Domain<?>> results;
+	private final Map<TerminologyObject, Domain> domains;
 
 	public EvalResult() {
-		results = new HashMap<String, Domain<?>>();
+		domains = new HashMap<TerminologyObject, Domain>();
 	}
 
-	@SuppressWarnings("unchecked")
-	public boolean add(String var, Domain<?> domain) {
-		@SuppressWarnings("rawtypes")
-		Domain oldDomain = results.get(var);
-		if (oldDomain != null) {
-			domain.addAll(oldDomain);
-		}
-		results.put(var, domain);
-		return true;
+	public EvalResult(TerminologyObject question, Domain domain) {
+		this();
+		domains.put(question, domain);
 	}
 
 	/**
-	 * Negates every Domain for every Variable in the EvalResult and returns an
-	 * new EvalResult
+	 * For use with CondOr
 	 * 
-	 * @created 08.05.2012
-	 * @return
+	 * @created 28.06.2012
+	 * @param result
 	 */
-	public EvalResult negate() {
-		EvalResult result = new EvalResult();
-		for (String key : results.keySet()) {
-			Domain<?> domain = results.get(key).negate();
-			result.add(key, domain);
+	public void mergeWith(EvalResult result) {
+		for (Map.Entry<TerminologyObject, Domain> entry : result.getEntries()) {
+			TerminologyObject q = entry.getKey();
+			Domain domain = entry.getValue();
+			if (!this.containsObject(q)) {
+				this.domains.put(q, domain);
+			} else {
+				Domain oldDomain = this.domains.remove(q);
+				this.domains.put(q, oldDomain.add(domain));
+
+			}
+			
 		}
-		// result.mergeAll();
-		return result;
+		
 	}
 
 	/**
-	 * intersects all Domains for every Variable in EvalResult with all Domains
-	 * of the equivalent Variable in eRes and returns an new EvalResult
-	 * Variables only appearing in one EvalResult are completely accepted
+	 * For use with CondAnd
 	 * 
-	 * @created 08.05.2012
-	 * @param eRes
-	 * @return
+	 * @created 28.06.2012
+	 * @param result
 	 */
-	@SuppressWarnings({
-			"unchecked", "rawtypes" })
-	public EvalResult intersect(EvalResult eRes) {
-		EvalResult result = new EvalResult();
-		Set<String> thisList = results.keySet();
-		thisList.removeAll(eRes.results.keySet());
-		for (String key : thisList) {
-			result.add(key, results.get(key));
-		}
-		Set<String> otherList = eRes.results.keySet();
-		otherList.removeAll(results.keySet());
-		for (String key : otherList) {
-			result.add(key, eRes.results.get(key));
+	public void intersectWith(EvalResult result) {
+		for (Map.Entry<TerminologyObject, Domain> entry : result.getEntries()) {
+			TerminologyObject q = entry.getKey();
+			Domain domain = entry.getValue();
+			if (!this.containsObject(q)) {
+				this.domains.put(q, domain);
+			}
+			else {
+				Domain oldDomain = this.domains.remove(q);
+				this.domains.put(q, oldDomain.intersect(domain));
 
-		}
-		for (String key1 : results.keySet()) {
-			for (String key2 : eRes.results.keySet()) {
-				if (key1.equals(key2)) {
-					Domain domain1 = results.get(key1);
-					Domain domain2 = eRes.results.get(key2);
-					if (domain1.intersects(domain2)) {
-						result.add(key1, domain1.intersectWith(domain2));
-					}
-				}
 			}
 
 		}
-		return result;
+
 	}
 
-	/**
-	 * 
-	 * @created 08.05.2012
-	 * @param eRes
-	 * @return
-	 */
-	@SuppressWarnings({
-			"rawtypes", "unchecked" })
-	public EvalResult restrictWith(EvalResult eRes) {
+	public void negate() {
 
-		EvalResult result = new EvalResult();
-		if (eRes.getVariables().isEmpty()) {
-			result = this;
+		for (TerminologyObject q : new LinkedList<TerminologyObject>(getObjects())) {
+			Domain domain = this.domains.get(q);
+			this.domains.put(q, domain.negate());
 		}
-		for (String key1 : results.keySet()) {
-			boolean contained = false;
 
-			for (String key2 : eRes.results.keySet()) {
-				if (key1.equals(key2)) {
-					contained = true;
-					Domain domain1 = results.get(key1);
-					Domain domain2 = eRes.results.get(key2);
-					if (domain1.intersects(domain2)) {
-						result.add(key1, domain1.intersectWith(domain2));
-					}
-				}
-			}
-			if (!contained) {
-				result.add(key1, results.get(key1));
-			}
-		}
-		return result;
 	}
 
-	/**
-	 * 
-	 * @created 08.05.2012
-	 * @param eRes
-	 * @return
-	 */
-	@SuppressWarnings({
-			"unchecked", "rawtypes" })
-	public boolean intersects(EvalResult eRes) {
-		for (String key1 : results.keySet()) {
-			for (String key2 : eRes.results.keySet()) {
-				if (key1.equals(key2)) {
-					Domain domain1 = results.get(key1);
-					Domain domain2 = eRes.results.get(key2);
-					if (domain1.intersects(domain2)) {
-						return true;
-					}
-				}
-			}
-		}
-		return false;
+	public boolean isEmpty() {
+		return domains.isEmpty();
 	}
 
-	/**
-	 * 
-	 * @created 08.05.2012
-	 * @param eRes
-	 * @return
-	 */
-	@SuppressWarnings({
-			"rawtypes", "unchecked" })
-	public boolean contains(EvalResult eRes) {
-		boolean contained = false;
-		for (String key1 : eRes.results.keySet()) {
-			for (String key2 : results.keySet()) {
-				if (key1.equals(key2)) {
-					Domain domain1 = results.get(key1);
-					Domain domain2 = eRes.results.get(key2);
-					if (domain1.contains(domain2)) {
-						return contained = true;
-					}
-				}
-			}
-			if (!contained) return false;
-		}
-		return true;
+	public boolean containsObject(TerminologyObject key) {
+		return domains.containsKey(key);
 	}
 
-	/**
-	 * merges all Domains for every Variable in EvalResult with all Domains of
-	 * the equivalent Variable in eRes and returns an new EvalResult
-	 * 
-	 * @created 08.05.2012
-	 * @param eRes
-	 * @return
-	 */
-	public EvalResult merge(EvalResult eRes) {
-		EvalResult result = new EvalResult();
-		for (String key : eRes.results.keySet()) {
-			result.add(key, eRes.results.get(key));
-		}
-		for (String key : results.keySet()) {
-			result.add(key, results.get(key));
-		}
-		return result;
+	public Domain getDomain(TerminologyObject key) {
+		return domains.get(key);
 	}
 
-	/**
-	 * 
-	 * @created 08.05.2012
-	 * @param eRes
-	 * @return
-	 */
-	@SuppressWarnings({
-			"rawtypes", "unchecked" })
-	public EvalResult substract(EvalResult eRes) {
-		EvalResult result = new EvalResult();
-		for (String key : results.keySet()) {
-			for (String key_eRes : eRes.results.keySet()) {
-				if (key.equals(key_eRes)) {
-					Domain domain1 = results.get(key_eRes);
-					Domain domain2 = eRes.results.get(key_eRes);
-					Domain difference = domain1.substract(domain2);
-					if (!difference.isEmpty()) {
-						result.add(key, difference);
-					}
-				}
-			}
-		}
-		return result;
+	public Set<TerminologyObject> getObjects() {
+		return domains.keySet();
 	}
 
-	/**
-	 *
-	 */
+	public Collection<Domain> getDomains() {
+		return domains.values();
+	}
+
+	public Set<Entry<TerminologyObject, Domain>> getEntries() {
+		return domains.entrySet();
+	}
+
 	@Override
-	public String toString() {
-		return "EvalResult: " + results.toString();
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((domains == null) ? 0 : domains.hashCode());
+		return result;
 	}
 
-	/**
-	 * 
-	 * @created 08.05.2012
-	 * @return
-	 */
-	public Set<String> getVariables() {
-		return results.keySet();
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) return true;
+		if (obj == null) return false;
+		if (getClass() != obj.getClass()) return false;
+		EvalResult other = (EvalResult) obj;
+		if (domains == null) {
+			if (other.domains != null) return false;
+		}
+		else if (!domains.equals(other.domains)) return false;
+		return true;
 	}
+
 }
