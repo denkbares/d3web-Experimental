1743048a9a2dbbafc5cfa49b67dd9f83f677405e
Sebastian Furth <sebastian.furth@denkbares.com>
2012-09-26 09:02:58 +0000

    fixed rendering of unordered Lists containing external links (these were misinterpreted as comments)

--- Research/KnowWE-Plugin-JSPWiki-MarkupSet/src/main/java/de/knowwe/jspwiki/types/ListType.java
+++ Research/KnowWE-Plugin-JSPWiki-MarkupSet/src/main/java/de/knowwe/jspwiki/types/ListType.java
@@ -3,8 +3,13 @@ package de.knowwe.jspwiki.types;
 import java.util.regex.Pattern;
 
 import de.knowwe.core.kdom.AbstractType;
+import de.knowwe.core.kdom.Type;
+import de.knowwe.core.kdom.basicType.CommentLineType;
 import de.knowwe.core.kdom.sectionFinder.RegexSectionFinder;
+import de.knowwe.kdom.dashtree.DashSubtree;
 import de.knowwe.kdom.dashtree.DashTree;
+import de.knowwe.kdom.dashtree.DashTreeElement;
+import de.knowwe.kdom.dashtree.LineEndComment;
 
 public class ListType extends AbstractType {
 
@@ -12,6 +17,24 @@ public class ListType extends AbstractType {
 		Pattern pattern = Pattern.compile("(^|\n+)((\\*).+?)(?=\n[^(\\*)])",
 				Pattern.MULTILINE + Pattern.DOTALL);
 		this.setSectionFinder(new RegexSectionFinder(pattern));
-		this.addChildType(new DashTree('*', 1));
+		this.addChildType(getNoCommentDashTree());
+	}
+
+	private Type getNoCommentDashTree() {
+
+		DashSubtree subtree = new DashSubtree('*', 1);
+		subtree.removeChildType(CommentLineType.class);
+		subtree.removeChildType(DashTreeElement.class);
+
+		DashTreeElement element = new DashTreeElement(subtree.getKey());
+		element.removeChildType(LineEndComment.class);
+		subtree.addChildType(element);
+
+		DashTree tree = new DashTree('*', 1);
+		tree.removeChildType(DashSubtree.class);
+		tree.removeChildType(CommentLineType.class);
+		tree.addChildType(subtree);
+
+		return tree;
 	}
 }
