2bd7bc7bee16dd311aa14a7660b7ec525039fbce
Jochen Reutelshöfer <jochen.reutelshoefer@denkbares.com>
2012-10-24 11:22:01 +0000

    fixed sectionizing of TurtleSubject with respect to blank nodes indicated by underscores

--- Research/KnowWE-Plugin-RDFS-TurtleN3/src/main/java/de/knowwe/kdom/n3/TurtleSubjectSection.java
+++ Research/KnowWE-Plugin-RDFS-TurtleN3/src/main/java/de/knowwe/kdom/n3/TurtleSubjectSection.java
@@ -1,17 +1,17 @@
 /*
  * Copyright (C) 2009 Chair of Artificial Intelligence and Applied Informatics
  * Computer Science VI, University of Wuerzburg
- *
+ * 
  * This is free software; you can redistribute it and/or modify it under the
  * terms of the GNU Lesser General Public License as published by the Free
  * Software Foundation; either version 3 of the License, or (at your option) any
  * later version.
- *
+ * 
  * This software is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
  * details.
- *
+ * 
  * You should have received a copy of the GNU Lesser General Public License
  * along with this software; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA, or see the FSF
@@ -27,6 +27,8 @@ import java.util.regex.Pattern;
 import de.knowwe.core.kdom.AbstractType;
 import de.knowwe.core.kdom.sectionFinder.RegexSectionFinder;
 import de.knowwe.kdom.AnonymousType;
+import de.knowwe.kdom.LeadingSpaces;
+import de.knowwe.kdom.TrailingSpaces;
 import de.knowwe.kdom.constraint.AtMostOneFindingConstraint;
 import de.knowwe.kdom.constraint.ConstraintSectionFinder;
 
@@ -36,10 +38,12 @@ public class TurtleSubjectSection extends AbstractType {
 		AnonymousType after = new AnonymousType("After");
 		after.setSectionFinder(new RegexSectionFinder("\\b[^\\s]*::"));
 		childrenTypes.add(after);
+		childrenTypes.add(new LeadingSpaces());
+		childrenTypes.add(new TrailingSpaces());
 		childrenTypes.add(new SubjectBNode());
 		childrenTypes.add(new TurtleSubject());
 		ConstraintSectionFinder c = new ConstraintSectionFinder(new RegexSectionFinder(
-				"(.*?)\\b[^\\s]*::", Pattern.DOTALL | Pattern.MULTILINE, 1));
+				"(^\\s.*?)\\b[^\\s]*::", Pattern.DOTALL | Pattern.MULTILINE, 1));
 		setSectionFinder(c);
 		c.addConstraint(AtMostOneFindingConstraint.getInstance());
 		// setCustomRenderer(new
