b9a3645dda2377f463e5758f7f876ec677fa51b9
Martina Freiberg <mfreiberg@d3web.svn.invalid>
2012-10-25 12:09:17 +0000

    Extended Tree Classification DecisionTreeKB Adapted Solution Panel to show only valued solutions (in a sorted manner)

--- Research/d3web-ProKEt/src/main/java/de/d3web/proket/d3web/output/render/SolutionPanelD3webRenderer.java
+++ Research/d3web-ProKEt/src/main/java/de/d3web/proket/d3web/output/render/SolutionPanelD3webRenderer.java
@@ -24,22 +24,24 @@ import de.d3web.core.knowledge.TerminologyObject;
 import de.d3web.core.knowledge.terminology.*;
 import de.d3web.core.session.Session;
 import de.d3web.core.session.blackboard.Blackboard;
-import de.d3web.indication.inference.PSMethodUserSelected;
 import de.d3web.proket.d3web.input.D3webConnector;
 import de.d3web.proket.d3web.utils.StringTemplateUtils;
+import java.util.ArrayList;
+import java.util.Collection;
 import javax.servlet.http.HttpSession;
 import org.antlr.stringtemplate.StringTemplate;
 
 /**
  * Basic class for rendering the solution panel or explanation component of a
  * d3web dialog
- * 
- * @author Martina Freiberg
- * @date Oct 2012
+ *
+ * @author Martina Freiberg @date Oct 2012
  */
 public class SolutionPanelD3webRenderer extends AbstractD3webRenderer {
 
-    /* some basic types of how the solution panel could be rendered */
+    /*
+     * some basic types of how the solution panel could be rendered
+     */
     public enum EXPLANATIONTYPE {
 
         TEXTUALLISTING,
@@ -50,110 +52,126 @@ public class SolutionPanelD3webRenderer extends AbstractD3webRenderer {
     }
 
     /**
-     * Entry point to solution panel rendering. According to given EXPL-TYPE
-     * the corresponding sub-method for rendering will be called.
+     * Entry point to solution panel rendering. According to given EXPL-TYPE the
+     * corresponding sub-method for rendering will be called.
+     *
      * @param d3webSession
      * @param type
      * @param http
-     * @return a String containing the rendered (HTML) representation of
-     * the solution panel
+     * @return a String containing the rendered (HTML) representation of the
+     * solution panel
      */
-    public String renderSolutionPanel(Session d3webSession, 
-            EXPLANATIONTYPE type, 
+    public String renderSolutionPanel(Session d3webSession,
+            EXPLANATIONTYPE type,
             HttpSession http) {
 
         StringBuilder bui = new StringBuilder();
 
         if (type == EXPLANATIONTYPE.TEXTUALLISTING) {
-            
+
             bui.append(getTextualListing(d3webSession));
-            
+
         } else if (type == EXPLANATIONTYPE.TABLE) {
-            
             // TODO
         } else if (type == EXPLANATIONTYPE.TREEMAP) {
-            
             // TODO
         } else if (type == EXPLANATIONTYPE.SOLUTIONGRAPH) {
-            
             // TODO
         } else if (type == EXPLANATIONTYPE.EXPLDIALOG) {
-            
             // TODO
         }
 
         return bui.toString();
     }
 
-   
     /**
      * Rendering method for getting the textual listing representation of a
      * solution panel
-     * 
+     *
      * @param d3websession
      * @return the (HTML) representation of the rendered solution panel in
      * textual listing form
      */
-     private String getTextualListing(Session d3websession) {
+    private String getTextualListing(Session d3websession) {
 
         KnowledgeBase kb = D3webConnector.getInstance().getKb();
         TerminologyObject rootSol = kb.getRootSolution();
         StringBuilder bui = new StringBuilder();
-        if (rootSol.getName().contains("000")) {
 
-            rootSol = rootSol.getChildren()[0];
-        }
-        
-        getSolutionsStates(rootSol, bui, d3websession.getBlackboard());
+        Collection<Solution> valuedSolutions =
+                d3websession.getBlackboard().getValuedSolutions();
+        Collection<Solution> sortedValuedSolutions =
+                sortSolutions(valuedSolutions, d3websession);
+
+        getSolutionStates(sortedValuedSolutions, bui, d3websession.getBlackboard());
 
         return bui.toString();
     }
 
-      private void getSolutionsStates(TerminologyObject solution, StringBuilder bui, 
-              Blackboard bb) {
+    private Collection<Solution> sortSolutions(Collection<Solution> valued,
+            Session d3websession) {
 
-        if (bb.getRating((Solution) solution, PSMethodUserSelected.getInstance()).equals(Rating.State.UNCLEAR)) {
-            
-        } else {
-                   
-            bui.append(getSolutionState((Solution)solution, bb));
+        Blackboard bb = d3websession.getBlackboard();
+        Collection<Solution> result = new ArrayList<Solution>();
+        ArrayList<Solution> established = new ArrayList<Solution>();
+        ArrayList<Solution> suggested = new ArrayList<Solution>();
+        ArrayList<Solution> excluded = new ArrayList<Solution>();
+
+        for (Solution s : valued) {
+            if (bb.getRating(s).getState().equals(Rating.State.ESTABLISHED)) {
+                established.add(s);
+            } else if (bb.getRating(s).getState().equals(Rating.State.SUGGESTED)) {
+                suggested.add(s);
+            } else if (bb.getRating(s).getState().equals(Rating.State.EXCLUDED)) {
+                excluded.add(s);
+            }
         }
-        if(solution.getChildren().length>0){
-        
-            for(TerminologyObject sol: solution.getChildren()){
-                getSolutionsStates(sol, bui, bb);
+
+        result.addAll(established);
+        result.addAll(suggested);
+        result.addAll(excluded);
+
+        return result;
+    }
+
+    private void getSolutionStates(Collection<Solution> sortedSols, StringBuilder bui,
+            Blackboard bb) {
+
+        for (Solution solution : sortedSols) {
+            if (!solution.getName().contains("000")) {
+
+                bui.append(renderSolutionState((Solution) solution, bb));
+
             }
         }
     }
-      
-      private String getSolutionState(Solution solution,  Blackboard bb) {
-        
-          // retrieve template
+
+    private String renderSolutionState(Solution solution, Blackboard bb) {
+
+        // retrieve template
         StringTemplate st = StringTemplateUtils.getTemplate("solutionPanel/Solution");
-        
+
         // fill template attribute
         st.setAttribute("solid", solution.getName());
         st.setAttribute("solutiontext", solution.getName());
-        
-        if(bb.getValuedSolutions().contains(solution)){
-         
-            if (bb.getRating(solution).getState().equals(Rating.State.ESTABLISHED)){
-                st.setAttribute("src", "/img/solEst.png");
-                st.setAttribute("alt", "established");
-                st.setAttribute("tt", "established");
-                
-            } else if (bb.getRating(solution).getState().equals(Rating.State.SUGGESTED)){
-                st.setAttribute("src", "/img/solSug.png");
-                st.setAttribute("alt", "suggested");
-                st.setAttribute("tt", "suggested");
-                
-            } else if (bb.getRating(solution).getState().equals(Rating.State.EXCLUDED)){
-                st.setAttribute("src", "/img/solExc.png");
-                st.setAttribute("alt", "excluded");
-                st.setAttribute("tt", "excluded");
-            }
+
+        if (bb.getRating(solution).getState().equals(Rating.State.ESTABLISHED)) {
+            st.setAttribute("src", "/img/solEst.png");
+            st.setAttribute("alt", "established");
+            st.setAttribute("tt", "established");
+
+        } else if (bb.getRating(solution).getState().equals(Rating.State.SUGGESTED)) {
+            st.setAttribute("src", "/img/solSug.png");
+            st.setAttribute("alt", "suggested");
+            st.setAttribute("tt", "suggested");
+
+        } else if (bb.getRating(solution).getState().equals(Rating.State.EXCLUDED)) {
+            st.setAttribute("src", "/img/solExc.png");
+            st.setAttribute("alt", "excluded");
+            st.setAttribute("tt", "excluded");
         }
-        
+
+
         return st.toString();
     }
 }
