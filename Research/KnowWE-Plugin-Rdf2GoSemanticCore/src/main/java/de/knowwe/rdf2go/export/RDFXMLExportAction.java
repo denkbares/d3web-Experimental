ec5ee45f9c48b6579b25fd44928981fc1f4b39fe
Albrecht Striffler <albrecht.striffler@denkbares.com>
2013-01-18 12:31:19 +0000

    Fixed Rdf2Go XML export to export proper UTF-8 encoded files.

--- Research/KnowWE-Plugin-Rdf2GoSemanticCore/src/main/java/de/knowwe/rdf2go/export/RDFXMLExportAction.java
+++ Research/KnowWE-Plugin-Rdf2GoSemanticCore/src/main/java/de/knowwe/rdf2go/export/RDFXMLExportAction.java
@@ -1,17 +1,17 @@
 /*
  * Copyright (C) 2011 Chair of Artificial Intelligence and Applied Informatics
  * Computer Science VI, University of Wuerzburg
- *
+ * 
  * This is free software; you can redistribute it and/or modify it under the
  * terms of the GNU Lesser General Public License as published by the Free
  * Software Foundation; either version 3 of the License, or (at your option) any
  * later version.
- *
+ * 
  * This software is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
  * details.
- *
+ * 
  * You should have received a copy of the GNU Lesser General Public License
  * along with this software; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA, or see the FSF
@@ -38,16 +38,17 @@ public class RDFXMLExportAction extends AbstractAction {
 	public void execute(UserActionContext context) throws IOException {
 		String filename = "ontoloy-export";
 
-		String mimetype = "application/rdf+xml";
+		String mimetype = "application/rdf+xml; charset=UTF-8";
 
 		context.setContentType(mimetype);
 		context.setHeader("Content-Disposition", "attachment; filename=\"" + filename
 				+ ".xml\"");
-		StringWriter string = new StringWriter();
-		Rdf2GoCore.getInstance().writeModel(string);
-		context.setContentLength(string.toString().length());
+		StringWriter stringWriter = new StringWriter();
+		Rdf2GoCore.getInstance().writeModel(stringWriter);
+		String string = stringWriter.toString();
+		context.setContentLength(string.length());
 
-		context.getWriter().write(string.toString());
+		context.getWriter().write(string);
 
 	}
 }
