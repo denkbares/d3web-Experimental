c0062dd2b3c55c7bc16f031257cdd789ef46cdf8
Albrecht Striffler <albrecht.striffler@denkbares.com>
2013-02-04 14:01:24 +0000

    Adapted ToolProvider...

--- Research/KnowWE-Plugin-TestcaseTable-Extensions/src/main/java/de/d3web/we/testcase/TestcaseTableEditProvider.java
+++ Research/KnowWE-Plugin-TestcaseTable-Extensions/src/main/java/de/d3web/we/testcase/TestcaseTableEditProvider.java
@@ -1,35 +1,63 @@
 /*
  * Copyright (C) 2012 University Wuerzburg, Computer Science VI
- *
- * This is free software; you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License as
- * published by the Free Software Foundation; either version 3 of
- * the License, or (at your option) any later version.
- *
- * This software is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this software; if not, write to the Free
- * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
- * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ * 
+ * This is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 3 of the License, or (at your option) any
+ * later version.
+ * 
+ * This software is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this software; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA, or see the FSF
+ * site: http://www.fsf.org.
  */
 package de.d3web.we.testcase;
 
-import de.knowwe.kdom.table.TableEditProvider;
+import de.knowwe.core.kdom.parsing.Section;
+import de.knowwe.core.kdom.parsing.Sections;
+import de.knowwe.core.user.UserContext;
+import de.knowwe.kdom.table.Table;
+import de.knowwe.tools.DefaultTool;
+import de.knowwe.tools.Tool;
+import de.knowwe.tools.ToolProvider;
 
 /**
  * 
  * @author Alexander Strehler
  * @created 29.01.2012
  */
-public class TestcaseTableEditProvider extends TableEditProvider {
+public class TestcaseTableEditProvider implements ToolProvider {
 
-	@Override
 	protected String getEditProviderCall() {
 		return "KNOWWE.plugin.testcasetable.editProvider";
+
+	}
+
+	@Override
+	public Tool[] getTools(Section<?> section, UserContext userContext) {
+		// edit the content of the markup, without %%Table and Annotations
+		Section<Table> content = Sections.findSuccessor(section, Table.class);
+		if (content == null) {
+			return new Tool[0];
+		}
+		else {
+
+			String editprovider = getEditProviderCall();
+			String jsAction = "KNOWWE.plugin.instantEdit.enable("
+					+ "'"
+					+ content.getID()
+					+ "',new KNOWWE.table.edit.Editor(" + editprovider + "));";
+			return new Tool[] { new DefaultTool(
+					"KnowWEExtension/images/pencil.png",
+					"Edit Table",
+					"Edit this table",
+					jsAction) };
+		}
 
 	}
 
