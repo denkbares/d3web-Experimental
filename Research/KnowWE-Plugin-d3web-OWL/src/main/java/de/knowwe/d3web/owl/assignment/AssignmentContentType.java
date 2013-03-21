6a8c0d3d6fbc67c5ed7ad162080f5ddf232c84bd
Albrecht Striffler <albrecht.striffler@denkbares.com>
2013-03-21 08:48:30 +0000

    Minor adaptation.

--- Research/KnowWE-Plugin-d3web-OWL/src/main/java/de/knowwe/d3web/owl/assignment/AssignmentContentType.java
+++ Research/KnowWE-Plugin-d3web-OWL/src/main/java/de/knowwe/d3web/owl/assignment/AssignmentContentType.java
@@ -1,29 +1,29 @@
 /*
  * Copyright (C) 2011 University Wuerzburg, Computer Science VI
- *
- * This is free software; you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License as
- * published by the Free Software Foundation; either version 3 of
- * the License, or (at your option) any later version.
- *
- * This software is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this software; if not, write to the Free
- * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
- * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ * 
+ * This is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 3 of the License, or (at your option) any
+ * later version.
+ * 
+ * This software is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this software; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA, or see the FSF
+ * site: http://www.fsf.org.
  */
 package de.knowwe.d3web.owl.assignment;
 
 import de.knowwe.core.kdom.AbstractType;
 import de.knowwe.core.kdom.basicType.CommentLineType;
-import de.knowwe.kdom.sectionFinder.AllTextFinderDivCorrectTrimmed;
+import de.knowwe.core.kdom.sectionFinder.AllTextFinderTrimmed;
 
 /**
- *
+ * 
  * @author Sebastian Furth
  * @created Mar 30, 2011
  */
@@ -31,7 +31,7 @@ public class AssignmentContentType extends AbstractType {
 
 	public AssignmentContentType() {
 		/* We take almost the whole content */
-		this.sectionFinder = new AllTextFinderDivCorrectTrimmed();
+		this.sectionFinder = new AllTextFinderTrimmed();
 		/* ChildrenTypes */
 		this.childrenTypes.add(new CommentLineType());
 		this.childrenTypes.add(new ChoiceValueAssignmentType());
