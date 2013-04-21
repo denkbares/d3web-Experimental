c9b2868bef24ad45f9bf4595a469cb53beb570ae
Christian Grotheer <grotheer@d3web.svn.invalid>
2013-04-21 14:45:33 +0000

    * moved TextDiffAction * renamed css file

--- Research/KnowWE-Plugin-Revisions/src/main/java/de/knowwe/revisions/diff/TextDiffAction.java
+++ Research/KnowWE-Plugin-Revisions/src/main/java/de/knowwe/revisions/diff/TextDiffAction.java
@@ -16,7 +16,7 @@
  * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA, or see the FSF
  * site: http://www.fsf.org.
  */
-package de.knowwe.revisions;
+package de.knowwe.revisions.diff;
 
 import java.io.IOException;
 import java.util.Arrays;
@@ -34,7 +34,7 @@ import difflib.Patch;
  * @author grotheer
  * @created 21.04.2013
  */
-public class ShowTextDiffAction extends AbstractAction {
+public class TextDiffAction extends AbstractAction {
 
 	@Override
 	public void execute(UserActionContext context) throws IOException {
