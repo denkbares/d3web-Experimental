45d084014327abd8eba5308adbc00c31966b76ac
Albrecht Striffler <albrecht.striffler@denkbares.com>
2013-04-23 07:40:35 +0000

    Adapted due to changes in flowchart.

--- Research/KnowWE-Plugin-DiaFlux-CoverageCity/src/main/java/de/knowwe/diaflux/coverageCity/CoverageCityRenderer.java
+++ Research/KnowWE-Plugin-DiaFlux-CoverageCity/src/main/java/de/knowwe/diaflux/coverageCity/CoverageCityRenderer.java
@@ -1,20 +1,20 @@
 /*
  * Copyright (C) 2013 University Wuerzburg, Computer Science VI
- *
- * This is free software; you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License as
- * published by the Free Software Foundation; either version 3 of
- * the License, or (at your option) any later version.
- *
- * This software is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this software; if not, write to the Free
- * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
- * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ * 
+ * This is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 3 of the License, or (at your option) any
+ * later version.
+ * 
+ * This software is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this software; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA, or see the FSF
+ * site: http://www.fsf.org.
  */
 package de.knowwe.diaflux.coverageCity;
 
@@ -23,7 +23,6 @@ import de.knowwe.core.kdom.rendering.RenderResult;
 import de.knowwe.core.kdom.rendering.Renderer;
 import de.knowwe.core.user.UserContext;
 import de.knowwe.diaflux.FlowchartUtils;
-
 
 /**
  * 
@@ -49,11 +48,10 @@ public class CoverageCityRenderer implements Renderer {
 		bob.appendHtml("<div id='pickresult" + id + "'></div>\n");
 		bob.appendHtml("<div id='nodeId" + id + "'></div>\n");
 
-		bob.append(FlowchartUtils.prepareFlowchartRenderer(user, "flowDisplay" + id,
+		bob.append(FlowchartUtils.prepareFlowchartRenderer(user, "flowDisplay" + id, null,
 				PathCoverageHighlight.COVERAGE_CITY_SCOPE, true));
 
 		bob.appendHtml("</div>\n");
 
 	}
-
 }
