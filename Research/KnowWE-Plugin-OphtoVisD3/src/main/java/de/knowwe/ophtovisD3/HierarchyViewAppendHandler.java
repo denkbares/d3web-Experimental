3c07a077fae153c358d46fabb4f64e68bf4354b1
Jochen Reutelshöfer <jochen.reutelshoefer@denkbares.com>
2013-05-31 07:34:36 +0000

    fixed html in HierarchyViewAppendHandler

--- Research/KnowWE-Plugin-OphtoVisD3/src/main/java/de/knowwe/ophtovisD3/HierarchyViewAppendHandler.java
+++ Research/KnowWE-Plugin-OphtoVisD3/src/main/java/de/knowwe/ophtovisD3/HierarchyViewAppendHandler.java
@@ -4,20 +4,21 @@ import de.knowwe.core.append.PageAppendHandler;
 import de.knowwe.core.kdom.rendering.RenderResult;
 import de.knowwe.core.user.UserContext;
 
-
 public class HierarchyViewAppendHandler implements PageAppendHandler {
 
 	@Override
 	public void append(String web, String topic, UserContext user, RenderResult result) {
-		if(DataBaseHelper.conceptIsInHierachy(topic)){
-		result.appendHtml("<div class=infobox>");
-		result.appendHtml("<div class=\"colapser\" onclick=\"collapseInfobox()\" >Infobox  ▲</div>");
-		result.appendHtml("<div id=\"chart\"></div>");
-		result.appendHtml("<script> createSidebarTree(\"" + topic + "\")</script>");
+		if (DataBaseHelper.conceptIsInHierachy(topic)) {
+			result.appendHtml("<div class=infobox>");
+			result.appendHtml("<div class=\"colapser\" onclick=\"collapseInfobox()\" >Infobox  ▲</div>");
+			result.appendHtml("<div id=\"chart\"></div>");
+			result.appendHtml("<script> createSidebarTree(\"" + topic + "\")</script>");
+			result.appendHtml("</div>");
 		}
-		//result.appendHtml("<script> var url =KNOWWE.core.util.getURL({action : 'ConnectionsAction', concept : \"" + topic + "\"});");
-		//result.appendHtml("renderConnections(url); </script>");
-		result.appendHtml("</Div>");
+		// result.appendHtml("<script> var url =KNOWWE.core.util.getURL({action : 'ConnectionsAction', concept : \""
+		// + topic + "\"});");
+		// result.appendHtml("renderConnections(url); </script>");
+
 	}
 
 	@Override
