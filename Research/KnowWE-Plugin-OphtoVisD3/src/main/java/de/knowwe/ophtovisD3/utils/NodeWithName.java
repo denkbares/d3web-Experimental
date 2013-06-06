9587bc3f74731a434ca3a449281104165ad329d7
Christian Henninger <chhenninger@gmail.com>
2013-06-06 20:42:23 +0000

    Merged changes into Project reactivated Project in Research pom

--- Research/KnowWE-Plugin-OphtoVisD3/src/main/java/de/knowwe/ophtovisD3/utils/NodeWithName.java
+++ Research/KnowWE-Plugin-OphtoVisD3/src/main/java/de/knowwe/ophtovisD3/utils/NodeWithName.java
@@ -1,46 +1,50 @@
 package de.knowwe.ophtovisD3.utils;
 
+import java.util.HashSet;
+
 import org.ontoware.rdf2go.model.node.URI;
 import org.ontoware.rdf2go.model.node.impl.URIImpl;
 
 import de.d3web.strings.Strings;
 import de.knowwe.rdf2go.Rdf2GoCore;
-import de.knowwe.wisskont.util.HierarchyNode;
+import de.knowwe.termbrowser.HierarchyProvider;
+import de.knowwe.termbrowser.util.HierarchyNode;
 import de.knowwe.wisskont.util.MarkupUtils;
 
 public class NodeWithName implements HierarchyNode<NodeWithName> {
 
-	 String name;
-	 String data;
-	 boolean highlighted;
+	String name;
+	String data;
+	boolean highlighted;
 
 	public NodeWithName(String name) {
 		this.name = name;
 	}
+
 	public NodeWithName(String name, String data) {
 		this.name = name;
-		this.data=data;
+		this.data = data;
 	}
+
 	public NodeWithName(String name, String data, boolean highlighted) {
 		this.name = name;
-		this.data=data;
-		this.highlighted=highlighted;
+		this.data = data;
+		this.highlighted = highlighted;
 	}
-	public NodeWithName(String name,boolean highlighted) {
+
+	public NodeWithName(String name, boolean highlighted) {
 		this.name = name;
 
-		this.highlighted=highlighted;
+		this.highlighted = highlighted;
 	}
-	
 
 	@Override
 	public int compareTo(NodeWithName o) {
-		// TODO implement
 		return this.name.compareTo(o.name);
 	}
 
 	@Override
-	public boolean isSubNodeOf(NodeWithName term) {
+	public boolean isSubNodeOf(NodeWithName term, HierarchyProvider hierarchy) {
 		if (name.equals(term.name)) return false;
 		String baseUrl = Rdf2GoCore.getInstance().getLocalNamespace();
 
@@ -52,14 +56,12 @@ public class NodeWithName implements HierarchyNode<NodeWithName> {
 		String otherURL = baseUrl + otherConceptURLString;
 		URI otherURI = new URIImpl(otherURL);
 
-		return MarkupUtils.isSubConceptOf(thisURI, otherURI);
+		return MarkupUtils.isSubConceptOf(thisURI, otherURI, new HashSet<URI>());
 	}
 
 	@Override
 	public String toString() {
 		return name;
 	}
-
-	
 
 }
