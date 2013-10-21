c2368558e8aee93bc9c009137a262b6fc0285ee9
Jochen Reutelshöfer <jochen.reutelshoefer@denkbares.com>
2013-10-21 12:55:11 +0000

    does not return null if no definition is found

--- Research/KnowWE-Plugin-IncrementalCompiler/src/main/java/de/knowwe/compile/utils/IncrementalCompilerLinkToTermDefinitionProvider.java
+++ Research/KnowWE-Plugin-IncrementalCompiler/src/main/java/de/knowwe/compile/utils/IncrementalCompilerLinkToTermDefinitionProvider.java
@@ -41,9 +41,6 @@ public class IncrementalCompilerLinkToTermDefinitionProvider implements de.knoww
 		if (termDefinitions.size() > 0) {
 			targetArticle = termDefinitions.iterator().next().getTitle();
 		}
-		else {
-			return null;
-		}
 
 		return createBaseURL() + "?page=" + targetArticle;
 	}
