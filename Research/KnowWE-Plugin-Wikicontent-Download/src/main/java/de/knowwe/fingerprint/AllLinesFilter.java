56f4bcd41286978f024119de639b1f214df12386
Albrecht Striffler <albrecht.striffler@denkbares.com>
2013-10-24 13:11:12 +0000

    Will be moved to KnowWE.

--- Research/KnowWE-Plugin-Wikicontent-Download/src/main/java/de/knowwe/fingerprint/AllLinesFilter.java
+++
@@ -1,9 +0,0 @@
-package de.knowwe.fingerprint;
-
-final class AllLinesFilter implements LineFilter {
-
-	@Override
-	public boolean accept(String line) {
-		return true;
-	}
-}
--- Research/KnowWE-Plugin-Wikicontent-Download/src/main/java/de/knowwe/fingerprint/LineFilter.java
+++
@@ -1,6 +0,0 @@
-package de.knowwe.fingerprint;
-
-public interface LineFilter {
-
-	boolean accept(String line);
-}
