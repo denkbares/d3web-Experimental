63b506eb603fc2ba64cb997740e7217bf0156e7b
Jochen Reutelshöfer <jochen.reutelshoefer@denkbares.com>
2013-11-15 15:39:46 +0000

    fixed NullPointer

--- Research/KnowWE-Plugin-d3web-TermBrowser/src/main/java/de/knowwe/d3web/browser/D3webHierarchyProvider.java
+++ Research/KnowWE-Plugin-d3web-TermBrowser/src/main/java/de/knowwe/d3web/browser/D3webHierarchyProvider.java
@@ -67,7 +67,7 @@ public class D3webHierarchyProvider implements HierarchyProvider {
 				Environment.DEFAULT_WEB, master);
 
 		Section<?> definingSection = terminologyManager.getTermDefiningSection(term);
-		if (!(definingSection.get() instanceof D3webTermDefinition)) return childrenList;
+		if (definingSection == null || !(definingSection.get() instanceof D3webTermDefinition)) return childrenList;
 		KnowledgeBase knowledgeBase = D3webUtils.getKnowledgeBase(Environment.DEFAULT_WEB, master);
 		de.d3web.core.knowledge.TerminologyManager manager = knowledgeBase.getManager();
 		NamedObject namedObject = manager.search(term.toExternalForm());
@@ -140,7 +140,7 @@ public class D3webHierarchyProvider implements HierarchyProvider {
 		TerminologyManager terminologyManager = Environment.getInstance().getTerminologyManager(
 				Environment.DEFAULT_WEB, master);
 		Section<?> definingSection = terminologyManager.getTermDefiningSection(term);
-		if (!(definingSection.get() instanceof D3webTermDefinition)) return parentList;
+		if (definingSection == null || !(definingSection.get() instanceof D3webTermDefinition)) return parentList;
 		KnowledgeBase knowledgeBase = D3webUtils.getKnowledgeBase(Environment.DEFAULT_WEB, master);
 		de.d3web.core.knowledge.TerminologyManager manager = knowledgeBase.getManager();
 		NamedObject namedObject = manager.search(term.toExternalForm());
