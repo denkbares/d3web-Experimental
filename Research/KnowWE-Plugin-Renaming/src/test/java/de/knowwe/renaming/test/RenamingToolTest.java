a27dedaa62e4e2da9d69aa4e744bf1a522974e44
Jochen Reutelshöfer <jochen.reutelshoefer@denkbares.com>
2014-01-09 08:45:04 +0000

    fixed and reactivated renaming tool test

--- Research/KnowWE-Plugin-Renaming/src/test/java/de/knowwe/renaming/test/RenamingToolTest.java
+++ Research/KnowWE-Plugin-Renaming/src/test/java/de/knowwe/renaming/test/RenamingToolTest.java
@@ -27,14 +27,12 @@ import java.util.HashMap;
 import java.util.Map;
 
 import junit.framework.TestCase;
-
-import org.junit.Ignore;
-
 import utils.TestUtils;
 import connector.DummyConnector;
 import de.d3web.plugin.test.InitPluginManager;
 import de.knowwe.core.Attributes;
 import de.knowwe.core.Environment;
+import de.knowwe.core.compile.Compilers;
 import de.knowwe.core.kdom.Article;
 import de.knowwe.core.kdom.RootType;
 import de.knowwe.renaming.taghandler.WordBasedRenameFinding;
@@ -42,12 +40,20 @@ import de.knowwe.renaming.taghandler.WordBasedRenamingAction;
 import de.knowwe.renaming.test.util.SplitObjectType;
 import de.knowwe.renaming.test.util.WordObjectType;
 
-@Ignore
+
 public class RenamingToolTest extends TestCase {
 
 	@Override
 	protected void setUp() throws IOException {
 		InitPluginManager.init();
+	}
+
+	private static void waitForCompilation() {
+		try {
+			Compilers.getCompilerManager(Environment.DEFAULT_WEB).awaitTermination();
+		}
+		catch (InterruptedException e) {
+		}
 	}
 
 	public void testFindings() {
@@ -72,7 +78,9 @@ public class RenamingToolTest extends TestCase {
 		rootType.addChildType(new WordObjectType());
 
 		TestUtils.processAndUpdateArticleJunit("TestUser", content1, "Test_Article1", "default_web");
+		waitForCompilation();
 		TestUtils.processAndUpdateArticleJunit("TestUser", content2, "Test_Article2", "default_web");
+		waitForCompilation();
 
 		/*
 		 * make the requests
