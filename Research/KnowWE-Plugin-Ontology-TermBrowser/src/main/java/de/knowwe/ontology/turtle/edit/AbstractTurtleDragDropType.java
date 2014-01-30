b9bbcc445d259948854dad9122be0018e3c6f16a
Sebastian Furth <sebastian.furth@denkbares.com>
2014-01-30 08:36:44 +0000

    * introduced AbstractTurtleDragDropType * TurtleDragDrop[Comma,Dot,Semicolon]Type extend AbstractTurtleDragDropType

--- Research/KnowWE-Plugin-Ontology-TermBrowser/src/main/java/de/knowwe/ontology/turtle/edit/AbstractTurtleDragDropType.java
+++ Research/KnowWE-Plugin-Ontology-TermBrowser/src/main/java/de/knowwe/ontology/turtle/edit/AbstractTurtleDragDropType.java
@@ -1,16 +1,16 @@
 /*
- * Copyright (C) 2014 denkbares GmbH
- * 
+ * Copyright (C) 2013 denkbares GmbH
+ *
  * This is free software; you can redistribute it and/or modify it under the
  * terms of the GNU Lesser General Public License as published by the Free
  * Software Foundation; either version 3 of the License, or (at your option) any
  * later version.
- * 
+ *
  * This software is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
  * details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this software; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA, or see the FSF
@@ -18,14 +18,21 @@
  */
 package de.knowwe.ontology.turtle.edit;
 
-/**
- * 
- * @author jochenreutelshofer
- * @created 21.01.2014
- */
-public class TurtleDragDropSemicolonType extends AbstractTurtleDragDropType {
+import de.knowwe.core.kdom.AbstractType;
+import de.knowwe.core.kdom.sectionFinder.RegexSectionFinder;
+import de.knowwe.kdom.renderer.CompositeRenderer;
+import de.knowwe.termbrowser.DroppableTargetSurroundingRenderer;
 
-	public TurtleDragDropSemicolonType() {
-		super(";");
+import java.util.regex.Pattern;
+
+/**
+ * @author Sebastian Furth (denkbares GmbH)
+ * @created 29.01.14
+ */
+public abstract class AbstractTurtleDragDropType extends AbstractType {
+
+	public AbstractTurtleDragDropType(String separatorChar) {
+		this.setSectionFinder(new RegexSectionFinder("\\s*"+ Pattern.quote(separatorChar) + "\\s*"));
+		this.setRenderer(new CompositeRenderer(new DroppableTargetSurroundingRenderer()));
 	}
 }
--- Research/KnowWE-Plugin-Ontology-TermBrowser/src/main/java/de/knowwe/ontology/turtle/edit/TurtleDragDropDotType.java
+++ Research/KnowWE-Plugin-Ontology-TermBrowser/src/main/java/de/knowwe/ontology/turtle/edit/TurtleDragDropDotType.java
@@ -1,15 +1,8 @@
 package de.knowwe.ontology.turtle.edit;
 
-import de.knowwe.core.kdom.AbstractType;
-import de.knowwe.kdom.renderer.CompositeRenderer;
-import de.knowwe.kdom.sectionFinder.StringSectionFinder;
-import de.knowwe.termbrowser.DroppableTargetSurroundingRenderer;
-
-
-public class TurtleDragDropDotType extends AbstractType {
+public class TurtleDragDropDotType extends AbstractTurtleDragDropType {
 
 	public TurtleDragDropDotType() {
-		this.setSectionFinder(new StringSectionFinder("."));
-		this.setRenderer(new CompositeRenderer(new DroppableTargetSurroundingRenderer()));
+		super(".");
 	}
 }
