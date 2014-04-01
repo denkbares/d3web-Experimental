0a261aaf3ae4a6ee2dc1e50b6258d787abb581be
Christian Henninger <chhenninger@gmail.com>
2014-04-01 14:03:03 +0000

    OphtoVisD3 now supports all relation-types

--- Research/KnowWE-Plugin-OphtoVisD3/src/main/java/de/knowwe/ophtovisD3/AjaxAction.java
+++ Research/KnowWE-Plugin-OphtoVisD3/src/main/java/de/knowwe/ophtovisD3/AjaxAction.java
@@ -5,35 +5,39 @@ import java.io.IOException;
 import de.knowwe.core.action.AbstractAction;
 import de.knowwe.core.action.UserActionContext;
 
-
 public class AjaxAction extends AbstractAction {
 
 	@Override
 	public void execute(UserActionContext context) throws IOException {
-		
+
 		GraphBuilder builder = new GraphBuilder();
-		String type, concept  ="";
+		String type, concept = "";
 		type = context.getParameter("type");
 		concept = context.getParameter("concept");
-		
-		if(type==null)
-			type="";
-		String responseString ="";
-		if(type.equals("force")){
-			responseString =builder.buildNamesandConnectionsJSON("Anamnese_Patientensituation", "unterkonzept");
-		}else if((type.equals("bubble"))){
-			 //responseString = GraphBuilder.buildGraph(concept, "unterkonzept", "temporalGraph",true);
-			 responseString = GraphBuilder.buildGraphExperimental("unterkonzept", concept);
-		}else{
+
+		if (type == null)
+			type = "";
+		String connectionType = context.getParameter("contype");
+		System.out.println("connectionType = " + connectionType);
+		if (connectionType == null) {
+			connectionType = "unterkonzept";
+		}
+		String responseString = "";
+		if (type.equals("force")) {
+			responseString = builder.buildNamesandConnectionsJSON("Anamnese_Patientensituation", connectionType);
+		}
+		else if ((type.equals("bubble"))) {
+			//responseString = GraphBuilder.buildGraph(concept, "unterkonzept", "temporalGraph",true);
+			responseString = GraphBuilder.buildGraphExperimental(connectionType, concept);
+		}
+		else {
 			// responseString = GraphBuilder.buildGraph(concept, "unterkonzept", "temporalGraph",false);
-			 responseString = GraphBuilder.buildGraphExperimental("unterkonzept", concept);
+			responseString = GraphBuilder.buildGraphExperimental(connectionType, concept);
 		}
 
 		context.setContentType("application/json; charset=UTF-8");
 		context.getWriter().write(responseString);
-		
-	
-		
+
 	}
 
 }
--- Research/KnowWE-Plugin-OphtoVisD3/src/main/java/de/knowwe/ophtovisD3/PartTreeAction.java
+++ Research/KnowWE-Plugin-OphtoVisD3/src/main/java/de/knowwe/ophtovisD3/PartTreeAction.java
@@ -5,16 +5,21 @@ import java.io.IOException;
 import de.knowwe.core.action.AbstractAction;
 import de.knowwe.core.action.UserActionContext;
 
-
 public class PartTreeAction extends AbstractAction {
 
 	@Override
 	public void execute(UserActionContext context) throws IOException {
 		String concept = context.getParameter("concept");
-		if(concept==null)
-			concept="";
-		
-		String responseString =GraphBuilder.buildPartTree(concept,"unterkonzept");
+		if (concept == null) {
+			concept = "";
+		}
+		String connectionType = context.getParameter("contype");
+		System.out.println("connectionType = " + connectionType);
+		System.out.println("test");
+		if (connectionType == null) {
+			connectionType = "unterkonzept";
+		}
+		String responseString = GraphBuilder.buildPartTree(concept, connectionType);
 		context.setContentType("application/json; charset=UTF-8");
 		context.getWriter().write(responseString);
 	}
