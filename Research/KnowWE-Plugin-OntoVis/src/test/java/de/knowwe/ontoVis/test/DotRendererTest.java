7e3fb592da7181395ce3882c80d52a14462d67cc
Jochen Reutelshöfer <jochen.reutelshoefer@denkbares.com>
2014-04-14 12:02:21 +0000

    fixed tests

--- Research/KnowWE-Plugin-OntoVis/src/test/java/de/knowwe/ontoVis/test/DotRendererTest.java
+++ Research/KnowWE-Plugin-OntoVis/src/test/java/de/knowwe/ontoVis/test/DotRendererTest.java
@@ -48,7 +48,7 @@ import static org.junit.Assert.assertEquals;
  * @created 23.05.2013
  */
 
-public class DotRendererTes {
+public class DotRendererTest {
 
 	static Rdf2GoCore rdfRepository = null;
 
