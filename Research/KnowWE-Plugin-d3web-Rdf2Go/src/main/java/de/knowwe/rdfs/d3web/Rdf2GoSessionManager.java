7f36312e3b190fb04fac3c4082debd63ee674969
Albrecht Striffler <albrecht.striffler@denkbares.com>
2014-07-21 15:53:33 +0000

    Some work towards prov-explanation with d3web-Rdf2Go and KnowSEC.

--- Research/KnowWE-Plugin-d3web-Rdf2Go/src/main/java/de/knowwe/rdfs/d3web/Rdf2GoSessionManager.java
+++ Research/KnowWE-Plugin-d3web-Rdf2Go/src/main/java/de/knowwe/rdfs/d3web/Rdf2GoSessionManager.java
@@ -8,32 +8,38 @@ import java.util.Set;
 import org.ontoware.rdf2go.model.Statement;
 import org.ontoware.rdf2go.model.node.BlankNode;
 import org.ontoware.rdf2go.model.node.Literal;
+import org.ontoware.rdf2go.model.node.Resource;
 import org.ontoware.rdf2go.model.node.URI;
 import org.ontoware.rdf2go.vocabulary.RDF;
 import org.ontoware.rdf2go.vocabulary.XSD;
 
+import de.d3web.core.inference.PSMethod;
 import de.d3web.core.knowledge.TerminologyObject;
 import de.d3web.core.session.Session;
 import de.d3web.core.session.Value;
 import de.d3web.core.session.blackboard.Fact;
+import de.knowwe.core.kdom.parsing.Section;
 import de.knowwe.rdf2go.Rdf2GoCore;
 import de.knowwe.rdf2go.utils.Rdf2GoUtils;
 
 public class Rdf2GoSessionManager {
 
 	private final Map<TerminologyObject, Set<Statement>> statementCache = new HashMap<TerminologyObject, Set<Statement>>();
+	private final Map<TerminologyObject, BlankNode> factNodeCache = new HashMap<TerminologyObject, BlankNode>();
+	private final Map<Object, Resource> agentNodeCache = new HashMap<Object, Resource>();
 
 	protected final Rdf2GoCore core;
 
 	public Rdf2GoSessionManager(Rdf2GoCore core) {
 		this.core = core;
+
+		// we make sure prov is available as a namespace
+		if (core.getNamespaces().get("prov") == null) {
+			core.addNamespace("prov", "http://www.w3.org/ns/prov#");
+		}
 	}
 
-	public void commit() {
-		this.core.commit();
-	}
-
-	public void addSessionToRdf2GoCore(Session session, boolean addPropagationListener, boolean commitAfterPropagation) {
+	public void addSessionToRdf2GoCore(Session session) {
 		Set<Statement> statements = new HashSet<Statement>();
 		URI sessionIdURI = Rdf2GoD3webUtils.getSessionIdURI(core, session);
 		URI sessionURI = core.createlocalURI(Session.class.getSimpleName());
@@ -60,36 +66,28 @@ public class Rdf2GoSessionManager {
 		// add value facts already present in the session
 		for (TerminologyObject valuedObject : session.getBlackboard().getValuedObjects()) {
 			Fact valueFact = session.getBlackboard().getValueFact(valuedObject);
-			addFactAsStatements(session, valuedObject, valueFact.getValue());
+			addFactAsStatements(session, valueFact);
 		}
 
 		// add statements to the core
 		core.addStatements(Rdf2GoUtils.toArray(statements));
-
-		// add propagation listener update the Rdf2GoCore with the changes in
-		// the session
-		if (addPropagationListener) {
-			session.getPropagationManager().addListener(
-					new Rdf2GoPropagationListener(this, commitAfterPropagation));
-		}
-
 	}
 
-	public void addFactAsStatements(Session session, TerminologyObject terminologyObject, Value value) {
+	public void addFactAsStatements(Session session, Fact fact) {
 
-		Set<Statement> statements = generateFactStatements(session, terminologyObject, value);
+		Set<Statement> statements = generateFactStatements(session, fact);
 
 		// add fact statements to cache
-		statementCache.put(terminologyObject, statements);
+		statementCache.put(fact.getTerminologyObject(), statements);
 
 		// add fact statements to the core
 		core.addStatements(Rdf2GoUtils.toArray(statements));
 	}
 
-	protected Set<Statement> generateFactStatements(Session session, TerminologyObject terminologyObject, Value value) {
+	protected Set<Statement> generateFactStatements(Session session, Fact fact) {
 		Set<Statement> statements = new HashSet<Statement>();
 
-		BlankNode factNode = core.createBlankNode();
+		BlankNode factNode = getFactNode(fact);
 
 		// blank node (Fact) rdf:type lns:Fact
 		Rdf2GoUtils.addStatement(core, factNode, RDF.type, Rdf2GoD3webUtils.getFactURI(core),
@@ -102,16 +100,74 @@ public class Rdf2GoSessionManager {
 
 		// blank node (Fact) lns:hasTerminologyObject lns:ObjectName
 		Rdf2GoUtils.addStatement(core, factNode,
-				Rdf2GoD3webUtils.getHasTerminologyObjectURI(core), terminologyObject.getName(),
+				Rdf2GoD3webUtils.getHasTerminologyObjectURI(core), fact.getTerminologyObject().getName(),
 				statements);
 
-		Literal valueLiteral = getValueLiteral(value);
+		Literal valueLiteral = getValueLiteral(fact.getValue());
 
 		// blank node (Fact) lns:hasValue "value"
 		Rdf2GoUtils.addStatement(core, factNode,
 				Rdf2GoD3webUtils.getHasValueURI(core), valueLiteral, statements);
 
+		// add PROV statements
+		Set<TerminologyObject> activeDerivationSources = fact.getPSMethod()
+				.getActiveDerivationSources(fact.getTerminologyObject(), session);
+
+		for (TerminologyObject activeDerivationSource : activeDerivationSources) {
+			Fact valueFact = session.getBlackboard().getValueFact(activeDerivationSource);
+			if (valueFact == null) continue;
+			BlankNode sourceFactNode = getFactNode(valueFact);
+			// blank node (Fact) prov:wasDerivedFrom lns:sourceFactNode
+			Rdf2GoUtils.addStatement(core, factNode, getProvURI("wasDerivedFrom"), sourceFactNode, statements);
+		}
+
+		// Resource (Fact) prov:wasAttributedTo blank node (Agent)
+		Resource agentNode = getAgentNode(fact);
+		Rdf2GoUtils.addStatement(core, factNode, getProvURI("wasAttributedTo"), agentNode, statements);
+
+		// blank node (Agent) rdf:type lns:PSMethod/Section...
+		Rdf2GoUtils.addStatement(core, agentNode, RDF.type, getAgentType(fact), statements);
+
 		return statements;
+	}
+
+	private URI getAgentType(Fact fact) {
+		Object source = fact.getSource();
+		if (source instanceof PSMethod) {
+			return core.createlocalURI(source.getClass().getSimpleName());
+		}
+		else if (source instanceof Section<?>) {
+			return core.createlocalURI(((Section) source).get().getName());
+		}
+		return core.createlocalURI(source.toString());
+	}
+
+	private Resource getAgentNode(Fact fact) {
+		Object source = fact.getSource();
+		Resource agentNode = agentNodeCache.get(source);
+		if (agentNode == null) {
+			if (source instanceof Section<?>) {
+				agentNode = core.createlocalURI(((Section) source).getID());
+			}
+			else {
+				agentNode = core.createBlankNode();
+			}
+			agentNodeCache.put(source, agentNode);
+		}
+		return agentNode;
+	}
+
+	private URI getProvURI(String name) {
+		return core.createURI("prov", name);
+	}
+
+	private BlankNode getFactNode(Fact fact) {
+		BlankNode factNode = factNodeCache.get(fact.getTerminologyObject());
+		if (factNode == null) {
+			factNode = core.createBlankNode();
+			factNodeCache.put(fact.getTerminologyObject(), factNode);
+		}
+		return factNode;
 	}
 
 	private Literal getValueLiteral(Value value) {
